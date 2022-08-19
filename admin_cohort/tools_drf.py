cb343fcc907f36e8e5492c02b45fe7d198ce22c4
Julien Dubiel <contact@juliendubiel.net>
2022-08-19 10:32:07 +0200

    Add some comment for CustomAuthSchema move to tools_drf.py

--- admin_cohort/tools_drf.py
+++ admin_cohort/tools_drf.py
@@ -1,6 +1,8 @@
 from drf_yasg.inspectors import SwaggerAutoSchema
 
 
+# Moved CustomAutoSchema class to a dedicated tools_drf.py file instead of tools.py to prevent circular dependency.
+
 # seen on https://stackoverflow.com/a/64440802
 class CustomAutoSchema(SwaggerAutoSchema):
     def get_tags(self, operation_keys=None):
