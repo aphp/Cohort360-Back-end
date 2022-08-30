711869c11e65765eaef9f48df1b45a41b6e90384
alexandreMartinEcl <alexandre.martin3@aphp.fr>
2022-08-30 18:47:11 +0200

    Splitted admin_cohort/urls with accesses/urls Kept old urls /accesses, /perimeters, etc. But needs front/fhir to change for /accesses/accesses, /accesses/perimeters, etc. dev/cohort360/gestion-de-projet#1444

--- admin_cohort/urls.py
+++ admin_cohort/urls.py
@@ -3,47 +3,15 @@ from django.urls import include, path
 from drf_yasg import openapi
 from rest_framework_extensions.routers import NestedRouterMixin
 
+from accesses.views import AccessViewSet, RoleViewSet, ProfileViewSet, \
+    PerimeterViewSet, NestedPerimeterViewSet
 from . import __version__, __title__
 
 from rest_framework import routers, permissions
-from rest_framework_swagger.views import get_swagger_view
 from drf_yasg.views import get_schema_view
 
-from accesses.views import RoleViewSet, AccessViewSet, PerimeterViewSet, \
-    ProfileViewSet, NestedPerimeterViewSet
-from .views import UserViewSet, LoggingViewset, maintenance_view, \
-    MaintenancePhaseViewSet
+from .views import UserViewSet, LoggingViewset, MaintenancePhaseViewSet
 
-# router = routers.DefaultRouter()
-
-
-class NestedDefaultRouter(NestedRouterMixin, routers.DefaultRouter):
-    pass
-
-
-router = NestedDefaultRouter()
-
-router.register(r'accesses', AccessViewSet, basename="accesses")
-router.register(r'maintenances',
-                MaintenancePhaseViewSet, basename="maintenances")
-
-p_router = router.register(
-    r'perimeters', PerimeterViewSet, basename="perimeters")
-p_router.register(
-    'children',
-    NestedPerimeterViewSet,
-    basename="perimeter-children",
-    parents_query_lookups=["parent"],
-)
-
-router.register(r'users', UserViewSet, basename="users")
-router.register(r'roles', RoleViewSet, basename="roles")
-router.register(r'profiles', ProfileViewSet, basename="profiles")
-router.register(r'logs', LoggingViewset, basename="logs")
-router.register(r'care-sites', PerimeterViewSet, basename="care-sites")
-router.register(r'providers', UserViewSet, basename="providers")
-
-old_schema_view = get_swagger_view(title='Cohort360 API')
 
 schema_view = get_schema_view(
     openapi.Info(
@@ -56,23 +24,45 @@ schema_view = get_schema_view(
     permission_classes=(permissions.AllowAny,),
 )
 
-internal_urls = [
-]
+
+class NestedDefaultRouter(NestedRouterMixin, routers.DefaultRouter):
+    pass
+
+
+router = NestedDefaultRouter()
+router.register(r'maintenances',
+                MaintenancePhaseViewSet, basename="maintenances")
+router.register(r'users', UserViewSet, basename="users")
+router.register(r'logs', LoggingViewset, basename="logs")
+# router.register(r'care-sites', PerimeterViewSet, basename="care-sites")
+# router.register(r'providers', UserViewSet, basename="providers")
+
+router.register(r'accesses', AccessViewSet, basename="accesses")
+router.register(r'roles', RoleViewSet, basename="roles")
+router.register(r'profiles', ProfileViewSet, basename="profiles")
+
+p_router = router.register(
+    r'perimeters', PerimeterViewSet, basename="perimeters")
+p_router.register(
+    'children',
+    NestedPerimeterViewSet,
+    basename="perimeter-children",
+    parents_query_lookups=["parent"],
+)
+
+internal_urls = []
+
 
 urlpatterns = [
     url(r'^', include(router.urls)),
-    path(
-        "workspaces/", include(
-            ('workspaces.urls', 'workspaces'), namespace="workspaces"
-        )
-    ),
-    path("exports/", include(('exports.urls', 'exports'), namespace="exports")),
-    path("cohort/", include(('cohort.urls', 'cohort'), namespace="cohort")),
-    path('accounts/', include('admin_cohort.urls_login')),
-    url(r'^docs', schema_view.with_ui('swagger', cache_timeout=0, )),
-    url(r'^old-docs', old_schema_view),
-    # to deprecate
-    url(r'maintenance', maintenance_view),
-    url(r'^redoc/$', schema_view.with_ui('redoc', cache_timeout=0),
-        name='schema-redoc'),
+    path("accounts/", include("admin_cohort.urls_login")),
+    path("accesses/",
+         include(("accesses.urls", "accesses"), namespace="accesses")),
+    path("cohort/", include(("cohort.urls", "cohort"), namespace="cohort")),
+    path("exports/", include(("exports.urls", "exports"), namespace="exports")),
+    path("workspaces/",
+         include(("workspaces.urls", "workspaces"), namespace="workspaces")),
+    url(r"^docs", schema_view.with_ui("swagger", cache_timeout=0, )),
+    url(r"^redoc/$", schema_view.with_ui("redoc", cache_timeout=0),
+        name="schema-redoc"),
 ]
