a91b4a72836767360df14a4af25da4883292fe5b
Emmanuel <emmanuel.herrmann@aphp.fr>
2022-09-20 10:10:32 +0200

    New nginx conf

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -19,6 +19,6 @@ celery worker -B -A admin_cohort --loglevel=info >> /app/log/celery.log 2>&1 &
 
 sleep 10
 
-python manage.py runserver 49040 >> /app/log/django.log 2>&1 &
+python manage.py runserver >> /app/log/django.log 2>&1 &
 
 tail -f /app/log/django.log
