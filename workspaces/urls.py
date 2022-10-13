02a9ec856276405b6a8fc58aaab6f2349ab69844
hicham <taroqhicham@gmail.com>
2022-10-13 17:49:24 +0200

    PEP 8

--- workspaces/urls.py
+++ workspaces/urls.py
@@ -9,14 +9,9 @@ from workspaces.views import AccountViewset, ProjectViewset, \
 router = routers.DefaultRouter()
 router.register(r'projects', ProjectViewset, basename="projects")
 router.register(r'users', AccountViewset, basename="accounts")
-router.register(
-    r'jupyter-machines', JupyterMachineViewset, basename="jupyter-machines")
-router.register(
-    r'ranger-hive-policies', RangerHivePolicyViewset,
-    basename="ranger-hive-policies")
+router.register(r'jupyter-machines', JupyterMachineViewset, basename="jupyter-machines")
+router.register(r'ranger-hive-policies', RangerHivePolicyViewset, basename="ranger-hive-policies")
 router.register(r'ldap-groups', LdapGroupViewset, basename="ldap-groups")
 router.register(r'kernels', KernelViewset, basename="kernels")
 
-urlpatterns = [
-    path('', include(router.urls)),
-]
+urlpatterns = [path('', include(router.urls))]
