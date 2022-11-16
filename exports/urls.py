4691d8067516a20e21167c41e58ab97b2a0f0549
hicham <hicham.taroq-ext@aphp.fr>
2022-11-16 15:26:10 +0100

    [3.7.0-SNAPSHOT] moved the empty route def to the end of routes list

--- exports/urls.py
+++ exports/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path, include
 from rest_framework import routers
 
-from exports.views import UnixAccountViewSet, CohortViewSet
+from exports.views import ExportRequestViewset, UnixAccountViewSet, CohortViewSet
 
 router = routers.DefaultRouter()
 router.register(r'unix-accounts', UnixAccountViewSet, basename="unix-accounts")
 router.register(r'cohorts', CohortViewSet, basename="cohorts")
+router.register(r'', ExportRequestViewset, basename="exports")
 
 urlpatterns = [path('', include(router.urls))]
