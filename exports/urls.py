c287b14e2a9682fa414e02fc88292d8d09726bd6
Thicham <taroqhicham@gmail.com>
2022-11-16 15:55:28 +0100

    [3.7.0-SNAPSHOT] removed empty route that prevents router from dispatching correctly (#66)
    * [3.7.0-SNAPSHOT] removed empty route that prevents router from dispatching correctly
    
    * [3.7.0-SNAPSHOT] moved the empty route def to the end of routes list
    
    * [3.7.0-SNAPSHOT] fixed typo
    
    Co-authored-by: hicham <hicham.taroq-ext@aphp.fr>

--- exports/urls.py
+++ exports/urls.py
@@ -3,10 +3,9 @@ from rest_framework import routers
 
 from exports.views import ExportRequestViewset, UnixAccountViewSet, CohortViewSet
 
-
 router = routers.DefaultRouter()
-router.register(r'', ExportRequestViewset, basename="exports")
 router.register(r'unix-accounts', UnixAccountViewSet, basename="unix-accounts")
 router.register(r'cohorts', CohortViewSet, basename="cohorts")
+router.register(r'', ExportRequestViewset, basename="exports")
 
 urlpatterns = [path('', include(router.urls))]
--- exports/views.py
+++ exports/views.py
@@ -81,39 +81,27 @@ class CohortViewSet(viewsets.ModelViewSet):
     lookup_field = "uuid"
     http_method_names = ["get"]
     serializer_class = AnnexeCohortResultSerializer
-    queryset = CohortResult.objects.filter(
-        request_job_status=JobStatus.finished
-    )
-
+    queryset = CohortResult.objects.filter(request_job_status=JobStatus.finished.value)
     swagger_tags = ['Exports - cohorts']
     filterset_class = CohortFilter
-    search_fields = ('$name', '$description',)
+    search_fields = ('$name', '$description')
 
     def get_permissions(self):
         return OR(AnnexesPermissions(), IsOwner())
 
     def get_queryset(self):
         user = self.request.user
-        if not can_review_transfer_jupyter(user)\
-                and not can_review_export_csv(user):
+        if not can_review_transfer_jupyter(user) and not can_review_export_csv(user):
             return self.queryset.filter(owner_id=user)
-
         return self.queryset
 
-    @swagger_auto_schema(
-        manual_parameters=list(map(
-            lambda x: openapi.Parameter(
-                name=x[0], in_=openapi.IN_QUERY, description=x[1], type=x[2],
-                pattern=x[3] if len(x) == 4 else None
-            ), [
-                ["owner_id", "Filter type",
-                 openapi.TYPE_STRING],
-                [
-                    "search",
-                    f"Will search in multiple fields "
-                    f"({', '.join(search_fields)})", openapi.TYPE_STRING
-                ],
-            ])))
+    @swagger_auto_schema(manual_parameters=list(map(lambda x: openapi.Parameter(name=x[0], in_=openapi.IN_QUERY,
+                                                                                description=x[1], type=x[2],
+                                                                                pattern=x[3] if len(x) == 4 else None),
+                                                    [["owner_id", "Filter type", openapi.TYPE_STRING],
+                                                     ["search", f"Will search in multiple "
+                                                                f"fields ({', '.join(search_fields)})",
+                                                      openapi.TYPE_STRING]])))
     def list(self, request, *args, **kwargs):
         return super(CohortViewSet, self).list(request, *args, **kwargs)
 
