10ac3a8fad68ebef3bc6bc132744159872973bd4
Emmanuel <emmanuel.herrmann@aphp.fr>
2022-11-22 10:56:37 +0100

    [1679] Add gunicorn to serve the application as WSGI

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -22,8 +22,4 @@ celery worker -B -A admin_cohort --loglevel=info >> /app/log/celery.log 2>&1 &
 
 sleep 10
 
-gunicorn --bind 8000 mysite.wsgi:application
-
-python manage.py runserver >> /app/log/django.log 2>&1 &
-
-tail -f /app/log/django.log
+gunicorn admin_cohort.wsgi
