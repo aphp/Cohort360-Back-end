ff75f1d70d5a76f2bacf13ac5102500137af1d1e
Thicham <taroqhicham@gmail.com>
2022-11-29 09:03:06 +0100

    [3.8.0-SNAPSHOT][1679] rearrange Gunicorn conf + move nginx conf (#73)
    * [3.8.0-SNAPSHOT][1679] rearrange Gunicorn conf + move nginx conf too. Ref. https://gitlab.eds.aphp.fr/dev/cohort360/gestion-de-projet/-/issues/1679
    
    * [3.8.0-SNAPSHOT][1679] attempts to wire logs Django and Gunicorn. Ref. https://gitlab.eds.aphp.fr/dev/cohort360/gestion-de-projet/-/issues/1679
    
    * [3.8.0-SNAPSHOT][1679] wired Django and Gunicorn logs Ref. https://gitlab.eds.aphp.fr/dev/cohort360/gestion-de-projet/-/issues/1679
    
    Co-authored-by: hicham <hicham.taroq-ext@aphp.fr>

---
+++ .conf/gunicorn.conf.py
@@ -0,0 +1,8 @@
+# bind = "127.0.0.1:8000"
+# workers = multiprocessing.cpu_count() * 2 + 1
+threads = 10
+
+# Whether to send Django output to the error log
+capture_output = True
+
+logconfig = ".conf/gunicorn.log.conf"
