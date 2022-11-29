ac46ccaa14977946eca0c775c3144714af2ed608
hicham <hicham.taroq-ext@aphp.fr>
2022-11-29 10:52:22 +0100

    [3.8.0-SNAPSHOT][1679] added some useful params for later use. Ref. https://gitlab.eds.aphp.fr/dev/cohort360/gestion-de-projet/-/issues/1679

--- .conf/gunicorn.conf.py
+++ .conf/gunicorn.conf.py
@@ -1,6 +1,7 @@
-# bind = "127.0.0.1:8000"
 # workers = multiprocessing.cpu_count() * 2 + 1
+# max_requests = 10
 threads = 10
+# bind = "127.0.0.1:8000"
 
 # Whether to send Django output to the error log
 capture_output = True
