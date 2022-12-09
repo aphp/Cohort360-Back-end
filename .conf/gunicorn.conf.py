c21054fb8e8b6b00bab8ec6e7d38ecad563b9e5a
hicham <hicham.taroq-ext@aphp.fr>
2022-12-09 09:21:37 +0100

    [3.9.0-SNAPSHOT] merge hotfix 3.8.2 - removed gunicorn max_requests

--- .conf/gunicorn.conf.py
+++ .conf/gunicorn.conf.py
@@ -2,7 +2,6 @@
 
 workers = 7
 threads = 10
-max_requests = 10   # maximum number of requests a worker will process before restarting
 
 capture_output = True   # Whether to send Django output to the error log
 accesslog = "/app/log/gunicorn.access.log"
