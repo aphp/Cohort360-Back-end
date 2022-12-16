07b8614088b3e776354c187fe157493fd9814d50
Emmanuel <emmanuel.herrmann@aphp.fr>
2022-12-16 10:40:55 +0100

    [3.9.0-SNAPSHOT][standalone/1758] Add logfile parameter

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -20,7 +20,7 @@ cron
 
 # See https://docs.celeryq.dev/en/stable/reference/cli.html#celery-worker for configuration
 #celery worker -beat -A admin_cohort --loglevel=INFO --logfile=/app/log/celery.log &
-celery worker -B -A admin_cohort --loglevel=info >> /app/log/celery.log 2>&1 &
+celery worker -B -A admin_cohort --loglevel=info  --logfile=/app/log/celery.log &
 sleep 10
 gunicorn admin_cohort.wsgi --config .conf/gunicorn.conf.py
 tail -f /app/log/gunicorn.log
