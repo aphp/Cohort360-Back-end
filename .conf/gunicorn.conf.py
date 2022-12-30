0387b99ebbdd7097f18274aa9de6fff9b644989e
hicham <hicham.taroq-ext@aphp.fr>
2022-12-30 10:16:45 +0100

    added rotating file handler for Gunicorn log files

--- .conf/gunicorn.conf.py
+++ .conf/gunicorn.conf.py
@@ -4,5 +4,51 @@ workers = 7
 threads = 10
 
 capture_output = True
-accesslog = "/app/log/gunicorn.access.log"
-errorlog = "/app/log/gunicorn.error.log"
+
+logconfig_dict = dict(
+    version=1,
+    disable_existing_loggers=False,
+    root={"level": "INFO", "handlers": ["console"]},
+    loggers={
+        "gunicorn.error": {
+            "level": "INFO",
+            "handlers": ["error_file"],
+            "propagate": True,
+            "qualname": "gunicorn.error"
+        },
+        "gunicorn.access": {
+            "level": "INFO",
+            "handlers": ["access_file"],
+            "propagate": True,
+            "qualname": "gunicorn.access"
+        }
+    },
+    handlers={
+        "console": {
+            "class": "logging.StreamHandler",
+            "formatter": "generic",
+            "stream": "ext://sys.stdout"
+        },
+        "access_file": {
+            "class": "logging.handlers.RotatingFileHandler",
+            "filename": "/app/log/gunicorn.access.log",
+            "maxBytes": 100 * 1024 * 1024,
+            "backupCount": 5,
+            "formatter": "generic",
+        },
+        "error_file": {
+            "class": "logging.handlers.RotatingFileHandler",
+            "filename": "/app/log/gunicorn.error.log",
+            "maxBytes": 100 * 1024 * 1024,
+            "backupCount": 5,
+            "formatter": "generic",
+        },
+    },
+    formatters={
+        "generic": {
+            "format": "%(asctime)s [%(process)d] [%(levelname)s] %(message)s",
+            "datefmt": "[%Y-%m-%d %H:%M:%S %z]",
+            "class": "logging.Formatter"
+        }
+    }
+)
