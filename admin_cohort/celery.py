b35642bef55b392b380afc6e3e0304b27388479a
hicham <hicham.taroq-ext@aphp.fr>
2023-01-11 18:01:54 +0100

    removed celery logging setup

--- admin_cohort/celery.py
+++ admin_cohort/celery.py
@@ -1,10 +1,8 @@
 from __future__ import absolute_import, unicode_literals
 
 import os
-from logging import INFO
-from logging.handlers import RotatingFileHandler
 
-from celery import Celery, signals
+from celery import Celery
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'admin_cohort.settings')
 
@@ -14,12 +12,12 @@ app.config_from_object('django.conf:settings', namespace='CELERY')
 app.autodiscover_tasks()
 
 
-@signals.after_setup_logger.connect
-def after_setup_celery_logger(logger, **kwargs):
-    logger.level = INFO
-    formatter = logger.handlers[0].formatter
-    handler = RotatingFileHandler(filename="log/celery.log",
-                                  maxBytes=100 * 1024 * 1024,
-                                  backupCount=1000)
-    handler.setFormatter(formatter)
-    logger.handlers = [handler]
+# @signals.after_setup_logger.connect
+# def after_setup_celery_logger(logger, **kwargs):
+#     logger.level = INFO
+#     formatter = logger.handlers[0].formatter
+#     handler = RotatingFileHandler(filename="log/celery.log",
+#                                   maxBytes=100 * 1024 * 1024,
+#                                   backupCount=1000)
+#     handler.setFormatter(formatter)
+#     logger.handlers = [handler]
