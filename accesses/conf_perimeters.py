db83d8e3b7bf0da30e165f420ddbd80d3a1110b2
hicham <hicham.taroq-ext@aphp.fr>
2023-02-07 11:22:00 +0100

    add cast to bigint on join clause

--- accesses/conf_perimeters.py
+++ accesses/conf_perimeters.py
@@ -164,7 +164,7 @@ def psql_query_care_site_relationship(top_care_site_ids: list) -> str:
             cd.cohort_size as cohort_size
             FROM omop.care_site cs
             INNER JOIN omop.cohort_definition cd
-            ON cd.owner_entity_id = cs.care_site_id
+            ON CAST(cd.owner_entity_id as BIGINT) = cs.care_site_id
             WHERE (cs.care_site_id IN ({str(top_care_site_ids)[1:-1]})
             AND cs.delete_datetime IS NULL
             AND cd.delete_datetime IS NULL AND cd.owner_domain_id = 'Care_site'
@@ -183,7 +183,7 @@ def psql_query_care_site_relationship(top_care_site_ids: list) -> str:
             INNER JOIN omop.fact_relationship frr
             ON css.care_site_id=frr.fact_id_1
             INNER JOIN omop.cohort_definition cd
-            ON cd.owner_entity_id = css.care_site_id
+            ON CAST(cd.owner_entity_id as BIGINT) = css.care_site_id
             WHERE (
             frr.fact_id_1!=frr.fact_id_2
             AND frr.domain_concept_id_1={cs_domain_concept_id}
