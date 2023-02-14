7bb15c1bab30c1decdc043f75cfa913af606962d
hicham <hicham.taroq-ext@aphp.fr>
2023-02-14 15:45:52 +0100

    fix method with no args

--- workspaces/permissions.py
+++ workspaces/permissions.py
@@ -2,6 +2,7 @@ from rest_framework import permissions
 
 from accesses.permissions import can_user_read_unix_accounts
 from admin_cohort.permissions import user_is_authenticated
+from workspaces.models import Account
 
 
 def has_user_one_unix_account(provider_source_value: str) -> bool:
@@ -9,7 +10,7 @@ def has_user_one_unix_account(provider_source_value: str) -> bool:
     return False
 
 
-def is_user_owner_of_unix_account():
+def is_user_owner_of_unix_account(provider_username: str, unix_account: Account):
     # will require request to API infra
     return False
 
@@ -19,8 +20,7 @@ class AccountPermissions(permissions.BasePermission):
         if not user_is_authenticated(request.user):
             return False
         return request.method in permissions.SAFE_METHODS and \
-            (can_user_read_unix_accounts(request.user.provider_username)
-             or has_user_one_unix_account(request.user.provider_username))
+            can_user_read_unix_accounts(request.user.provider_username)    # or has_user_one_unix_account(request.user.provider_username))
         # when managing will be requested
         # return \
         #     request.method in permissions.SAFE_METHODS \
@@ -32,8 +32,7 @@ class AccountPermissions(permissions.BasePermission):
         if not user_is_authenticated(request.user):
             return False
         if request.method in permissions.SAFE_METHODS:
-            return can_user_read_unix_accounts(request.user) \
-                or is_user_owner_of_unix_account(request.user.provider_username, obj)
+            return can_user_read_unix_accounts(request.user)    # or is_user_owner_of_unix_account(request.user.provider_username, obj)
         else:
             return False
             # when managing will be requested
