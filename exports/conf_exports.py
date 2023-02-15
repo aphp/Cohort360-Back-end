ec0875b221e2f1d915f4f351e70b16cb8c48703b
hicham <hicham.taroq-ext@aphp.fr>
2023-02-15 11:30:50 +0100

    use specific exceptions, phase 2

--- exports/conf_exports.py
+++ exports/conf_exports.py
@@ -14,6 +14,7 @@ from rest_framework.exceptions import ValidationError
 
 from admin_cohort.models import JobStatus
 from admin_cohort.tools import prettify_dict
+from admin_cohort.types import MissingDataError
 from .models import ExportRequest
 from .types import ApiJobResponse, HdfsServerUnreachableError, ExportType
 
@@ -37,7 +38,7 @@ HIVE_EXPORTER_USER = env.get('HIVE_EXPORTER_USER')
 OMOP_ENVIRONMENT = env.get('EXPORT_OMOP_ENVIRONMENT')
 
 
-class ApiJobStatutes(enum.Enum):
+class ApiJobStatuses(enum.Enum):
     pending = 'PENDING'     # Task state is unknown (assumed pending since you know the id).
     received = 'RECEIVED'   # Task was received by a worker (only used in events).
     started = 'STARTED'     # Task was started by a worker (:setting:`task_track_started`).
@@ -49,15 +50,15 @@ class ApiJobStatutes(enum.Enum):
     ignored = 'IGNORED'
 
 
-dct_api_to_job_status = {ApiJobStatutes.pending.value: JobStatus.pending,
-                         ApiJobStatutes.received.value: JobStatus.pending,
-                         ApiJobStatutes.started.value: JobStatus.started,
-                         ApiJobStatutes.success.value: JobStatus.finished,
-                         ApiJobStatutes.failure.value: JobStatus.failed,
-                         ApiJobStatutes.revoked.value: JobStatus.cancelled,
-                         ApiJobStatutes.rejected.value: JobStatus.failed,
-                         ApiJobStatutes.retry.value: JobStatus.pending,
-                         ApiJobStatutes.ignored.value: JobStatus.failed}
+dct_api_to_job_status = {ApiJobStatuses.pending.value: JobStatus.pending,
+                         ApiJobStatuses.received.value: JobStatus.pending,
+                         ApiJobStatuses.started.value: JobStatus.started,
+                         ApiJobStatuses.success.value: JobStatus.finished,
+                         ApiJobStatuses.failure.value: JobStatus.failed,
+                         ApiJobStatuses.revoked.value: JobStatus.cancelled,
+                         ApiJobStatuses.rejected.value: JobStatus.failed,
+                         ApiJobStatuses.retry.value: JobStatus.pending,
+                         ApiJobStatuses.ignored.value: JobStatus.failed}
 
 # TOOLS ###############################################################
 
@@ -75,9 +76,9 @@ def build_location(db_name: str) -> str:
 class JobResult:
     def __init__(self, **kwargs):
         try:
-            self.status: ApiJobStatutes = ApiJobStatutes[kwargs.get('status')]
+            self.status: ApiJobStatuses = ApiJobStatuses[kwargs.get('status')]
         except ValueError:
-            raise Exception(f"Status received from Infra API is not expected: {kwargs.get('status')}")
+            raise
         self.ret_code: int = kwargs.get('ret_code')
         self.out: str = kwargs.get('out')
         self.err: str = kwargs.get('err')
@@ -87,7 +88,7 @@ class JobStatusResponse:
     def __init__(self, **kwargs):
         self.task_status: str = kwargs.get('task_status')
         if 'task_result' not in kwargs:
-            raise Exception(f"Response from Infra API is missing 'task_result'. What was received : {kwargs}")
+            raise MissingDataError(f"Response from Infra API is missing 'task_result'. Received: {prettify_dict(kwargs)}")
         if kwargs.get('task_result'):
             self.task_result: JobResult = JobResult(**kwargs.get('task_result'))
         else:
@@ -97,9 +98,7 @@ class JobStatusResponse:
 class PostJobResponse:
     def __init__(self, **kwargs):
         if 'task_id' not in kwargs:
-            raise Exception(f"Response from Infra API not expected: "
-                            f"missing 'task_id' - "
-                            f"received : {prettify_dict(kwargs)}")
+            raise MissingDataError(f"Response from Infra API is missing 'task_id'. Received: {prettify_dict(kwargs)}")
         self.task_id: str = kwargs.get('task_id')
 
 
