10c77ccbc0619458b9106646e8d063fdcbf0feb8
hicham <hicham.taroq-ext@aphp.fr>
2023-05-02 15:20:13 +0200

    refactor: rename variables

--- accesses/tasks.py
+++ accesses/tasks.py
@@ -7,7 +7,7 @@ from django.utils import timezone
 from accesses.models import Access, Profile
 from admin_cohort import celery_app
 from admin_cohort.models import User
-from admin_cohort.settings import ACCESS_EXPIRY_FIRST_ALERT_IN_DAYS, EMAIL_SENDER_ADDRESS, ACCESS_EXPIRY_SECOND_ALERT_IN_DAYS
+from admin_cohort.settings import ACCESS_EXPIRY_FIRST_ALERT_IN_DAYS, ACCESS_EXPIRY_SECOND_ALERT_IN_DAYS, EMAIL_SENDER_ADDRESS
 
 KEY_NAME = "KEY_NAME"
 KEY_EXPIRY_DAYS = "KEY_EXPIRY_DAYS"
@@ -38,9 +38,9 @@ def send_alert_email(user: User, in_days: int):
 
 
 def send_access_expiry_alerts(days: int):
-    accesses_expiry_date = date.today() + timedelta(days=days)
-    expiring_accesses = Access.objects.filter(Q(end_datetime__date=accesses_expiry_date) |
-                                              Q(manual_end_datetime__date=accesses_expiry_date))\
+    expiry_date = date.today() + timedelta(days=days)
+    expiring_accesses = Access.objects.filter(Q(end_datetime__date=expiry_date) |
+                                              Q(manual_end_datetime__date=expiry_date))\
                                       .values("profile")\
                                       .annotate(total=Count("profile"))
     for access in expiring_accesses:
@@ -50,10 +50,6 @@ def send_access_expiry_alerts(days: int):
 
 @celery_app.task()
 def check_expiring_accesses():
-    print("**************************")
-    if timezone.now().hour != 9:
-        print("************************** not 10h")
-        return
-    print("************************** 10h")
-    send_access_expiry_alerts(days=ACCESS_EXPIRY_FIRST_ALERT_IN_DAYS)
-    send_access_expiry_alerts(days=ACCESS_EXPIRY_SECOND_ALERT_IN_DAYS)
+    if timezone.now().hour == 7:
+        send_access_expiry_alerts(days=ACCESS_EXPIRY_FIRST_ALERT_IN_DAYS)
+        send_access_expiry_alerts(days=ACCESS_EXPIRY_SECOND_ALERT_IN_DAYS)
