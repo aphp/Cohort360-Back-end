c539a67d94107a0c80e70fb344038ddb9ed7630c
hicham <hicham.taroq-ext@aphp.fr>
2023-05-12 18:41:40 +0200

    ci: add login tests

--- admin_cohort/tests/tests_login.py
+++ admin_cohort/tests/tests_login.py
@@ -1,22 +1,52 @@
+from unittest import mock
+from unittest.mock import MagicMock
 
 from rest_framework import status
 from rest_framework.test import APITestCase
+
+from admin_cohort.models import User
+from admin_cohort.types import JwtTokens, LoginError, ServerError
+
+
+def create_regular_user() -> User:
+    return User.objects.create(firstname="Regular",
+                               lastname="USER",
+                               email="regular.user@aphp.fr",
+                               provider_username="12345")
 
 
 class LoginTests(APITestCase):
 
     def setUp(self):
         self.login_url = '/accounts/login/'
-        self.login_data = {"username": "userx", "password": "psswd"}
+        self.regular_user = create_regular_user()
+        self.unregistered_user_credentials = {"username": "spy-user",
+                                              "password": "top-secret-007"}
 
-    def test_login_with_wrong_credentials(self):
-        response = self.client.post(path=self.login_url, data=self.login_data, format='json')
+    def test_login_with_unregistered_user(self):
+        response = self.client.post(path=self.login_url, data=self.unregistered_user_credentials)
         self.assertEqual(response.status_code, status.HTTP_401_UNAUTHORIZED)
 
-    # WIP
-    # @mock.patch("admin_cohort.views.LoginView.post")
-    # def test_login_unavailable_jwt(self, mock_super_post: MagicMock):
-    #     mock_super_post.side_effect = ServerError("JWT server unavailable")
-    #     response = self.client.post(path=self.login_url, data=self.login_data, format='json')
-    #     mock_super_post.assert_called()
-    #     self.assertEqual(response.status_code, status.HTTP_503_SERVICE_UNAVAILABLE)
+    @mock.patch("admin_cohort.auth.auth_backend.get_jwt_tokens")
+    def test_login_with_wrong_credentials(self, mock_get_tokens: MagicMock):
+        mock_get_tokens.side_effect = LoginError("Invalid username or password")
+        response = self.client.post(path=self.login_url, data={"username": self.regular_user.provider_username,
+                                                               "password": "wrong-psswd"})
+        mock_get_tokens.assert_called()
+        self.assertEqual(response.status_code, status.HTTP_401_UNAUTHORIZED)
+
+    @mock.patch("admin_cohort.auth.auth_backend.get_jwt_tokens")
+    def test_login_unavailable_jwt_server(self, mock_get_tokens: MagicMock):
+        mock_get_tokens.side_effect = ServerError("JWT server unavailable")
+        response = self.client.post(path=self.login_url, data={"username": self.regular_user.provider_username,
+                                                               "password": "psswd"})
+        mock_get_tokens.assert_called()
+        self.assertEqual(response.status_code, status.HTTP_401_UNAUTHORIZED)
+
+    @mock.patch("admin_cohort.auth.auth_backend.get_jwt_tokens")
+    def test_login_success(self, mock_get_tokens: MagicMock):
+        mock_get_tokens.return_value = JwtTokens(access="aaa", refresh="rrr")
+        response = self.client.post(path=self.login_url, data={"username": self.regular_user.provider_username,
+                                                               "password": "any-will-do"})
+        mock_get_tokens.assert_called()
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
