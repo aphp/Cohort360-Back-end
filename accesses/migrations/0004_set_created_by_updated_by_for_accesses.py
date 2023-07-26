36fa73e650752e30822e76c2bbf89c4054d74b82
hicham <hicham.taroq-ext@aphp.fr>
2023-07-26 14:36:31 +0200

    fix(migration): fix dependency

--- accesses/migrations/0004_set_created_by_updated_by_for_accesses.py
+++ accesses/migrations/0004_set_created_by_updated_by_for_accesses.py
@@ -53,7 +53,7 @@ class Migration(migrations.Migration):
     dependencies = [
         migrations.swappable_dependency(settings.AUTH_USER_MODEL),
         ('accesses', '0003_access_created_by_access_updated_by'),
-        ('rest_framework_tracking', '0011_auto_20201117_2016.py')
+        ('rest_framework_tracking', '0011_auto_20201117_2016')
     ]
 
     operations = [
