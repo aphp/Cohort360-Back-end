f53b50200411150e544955eb10efa7c084beca02
anddy <anddy.seruga@aphp.fr>
2023-08-08 17:48:41 +0200

    feat: Add Fhir Filter model

---
+++ cohort/models/fhir_filter.py
@@ -0,0 +1,20 @@
+from django.core.validators import MinLengthValidator, MaxLengthValidator
+
+from admin_cohort.models import User
+from cohort.models import CohortBaseModel
+from django.db import models
+
+
+class FhirFilter(CohortBaseModel):
+    """
+    From this following UML:
+    https://gitlab.eds.aphp.fr/dev/cohort360/gestion-de-projet/-/issues/2200
+    """
+    fhir_resource = models.CharField(max_length=255)
+    fhir_version = models.CharField(max_length=50)
+    filter_name = models.CharField(validators=[MinLengthValidator(2), MaxLengthValidator(50)])
+    fhir_filter = models.TextField()
+    owner = models.ForeignKey(User, on_delete=models.CASCADE, related_name='user_fhir_filters')
+
+    class Meta:
+        unique_together = ('fhir_resource', 'filter_name', 'owner_id')
