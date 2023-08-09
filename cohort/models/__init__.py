f9e0cf0b3206e3778c4309a7fc0ac6da6a8d4ee7
anddy <anddy.seruga@aphp.fr>
2023-08-09 10:33:33 +0200

    feat: Add Fhir Filter model in __init__

--- cohort/models/__init__.py
+++ cohort/models/__init__.py
@@ -4,5 +4,6 @@ from .request import Request
 from .request_query_snapshot import RequestQuerySnapshot
 from .dated_measure import DatedMeasure
 from .cohort_result import CohortResult
+from .fhir_filter import FhirFilter
 
 __all__ = ["CohortBaseModel", "Folder", "Request", "RequestQuerySnapshot", "DatedMeasure", "CohortResult"]
