2428a49db751205247736b8b68231410b5a1c400
anddy <anddy.seruga@aphp.fr>
2023-08-10 11:36:04 +0200

    test: add test calling base list url

--- cohort/tests/tests_view_fhir_filter.py
+++ cohort/tests/tests_view_fhir_filter.py
@@ -1,3 +1,5 @@
+from django.urls import reverse
+
 from cohort.tests.cohort_app_tests import CohortAppTests
 
 
@@ -5,3 +7,7 @@ class TestFhirFilterAPI(CohortAppTests):
 
     def test_always_true(self):
         assert True
+
+    def test_url_reverse(self):
+        url = reverse("cohort:fhir-filters-list")
+        assert url == "/cohort/fhir-filters/"
