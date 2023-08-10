72ffd4a32db006a9b049f5f8d222f9b5ed77dcd8
anddy <anddy.seruga@aphp.fr>
2023-08-10 11:36:35 +0200

    test: add test testing the get list route

--- cohort/tests/tests_view_fhir_filter.py
+++ cohort/tests/tests_view_fhir_filter.py
@@ -1,9 +1,13 @@
 from django.urls import reverse
 
 from cohort.tests.cohort_app_tests import CohortAppTests
+from rest_framework.test import force_authenticate
+
+from cohort.views.fhir_filter import FhirFilterViewSet
 
 
 class TestFhirFilterAPI(CohortAppTests):
+    list_view = FhirFilterViewSet.as_view({'get': 'list'})
 
     def test_always_true(self):
         assert True
@@ -11,3 +15,10 @@ class TestFhirFilterAPI(CohortAppTests):
     def test_url_reverse(self):
         url = reverse("cohort:fhir-filters-list")
         assert url == "/cohort/fhir-filters/"
+
+    def test_list_route_callable(self):
+        url = reverse("cohort:fhir-filters-list")
+        request = self.factory.get(url)
+        force_authenticate(request, self.user1)
+        response = self.__class__.list_view(request)
+        assert response.status_code == 200
