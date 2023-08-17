c024a4fb49196a6444a9180ced822e53b1fa7990
anddy <anddy.seruga@aphp.fr>
2023-08-17 14:39:35 +0200

    fix: circular import

--- cohort/models/__init__.py
+++ cohort/models/__init__.py
@@ -1,9 +1,9 @@
 from .cohort_basemodel import CohortBaseModel
-from .cohort_result import CohortResult
-from .dated_measure import DatedMeasure
-from .fhir_filter import FhirFilter
 from .folder import Folder
 from .request import Request
 from .request_query_snapshot import RequestQuerySnapshot
+from .dated_measure import DatedMeasure
+from .cohort_result import CohortResult
+from .fhir_filter import FhirFilter
 
 __all__ = ["CohortBaseModel", "Folder", "Request", "RequestQuerySnapshot", "DatedMeasure", "CohortResult", "FhirFilter"]
