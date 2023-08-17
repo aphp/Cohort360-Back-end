153298e39fb95ef6da38757507ff7799299804fb
hicham <hicham.taroq-ext@aphp.fr>
2023-08-17 18:17:05 +0200

    feat: dispatch on changing perimeter's allowed_users

--- accesses/signals.py
+++ accesses/signals.py
@@ -5,45 +5,73 @@ onchange_allowed_users = Signal()
 
 
 def manage_onchange_allowed_users(sender, **kwargs):
-    print("************ allowed users changed!")
     operation = kwargs.get("operation")
     perimeter = kwargs.get("perimeter")
     user_id = kwargs.get("user_id")
 
     if operation == "add":
-        print("************ add")
         add_new_allowed_user(perimeter=perimeter, user_id=user_id)
     elif operation == "revoke":
-        print("************ revoke")
         revoke_user(perimeter=perimeter, user_id=user_id)
 
 
 def add_new_allowed_user(perimeter, user_id: int):
     perimeter.allowed_users = list(set((perimeter.allowed_users or []) + [user_id]))
     perimeter.save()
-    while perimeter.parent:
-        allowed_users_inferior_levels = list(set((perimeter.allowed_users_inferior_levels or []) +
-                                                 perimeter.allowed_users))
-        parent = perimeter.parent
-        parent_allowed_users_inferior_levels = parent.allowed_users_inferior_levels or []
-        parent.allowed_users_inferior_levels = list(set(parent_allowed_users_inferior_levels +
-                                                        allowed_users_inferior_levels))
+    add_user_to_parent_perimeters(perimeter=perimeter, user_id=user_id)
+    add_user_to_child_perimeters(perimeter=perimeter, user_id=user_id)
+
+
+def add_user_to_parent_perimeters(perimeter, user_id):
+    parent = perimeter.parent
+    if parent:
+        parent.allowed_users_inferior_levels = list(set((parent.allowed_users_inferior_levels or []) + [user_id]))
         parent.save()
-        perimeter = parent
+        if parent.parent:
+            add_user_to_parent_perimeters(perimeter=parent, user_id=user_id)
+
+
+def add_user_to_child_perimeters(perimeter, user_id):
+    for child in perimeter.children.all():
+        allowed_users_above_levels = child.allowed_users_above_levels or []
+        child.allowed_users_above_levels = list(set(allowed_users_above_levels + [user_id]))
+        child.save()
+        if child.children.all():
+            revoke_user_from_child_perimeters(perimeter=child, user_id=user_id)
 
 
 def revoke_user(perimeter, user_id: int):
-    perimeter.allowed_users.remove(user_id)
-    perimeter.save()
+    try:
+        perimeter.allowed_users.remove(user_id)
+        perimeter.save()
+    except ValueError:
+        return
+    revoke_user_from_parent_perimeters(perimeter=perimeter, user_id=user_id)
 
-    while perimeter.parent:
-        parent = perimeter.parent
-        remove_user = True
-        for child in parent.children.all():
-            if user_id in child.allowed_users + child.allowed_users_inferior_levels:
-                remove_user = False
-                break
-        if remove_user:
-            parent.allowed_users_inferior_levels.remove(user_id)
-            parent.save()
-        perimeter = parent
+    if user_id not in perimeter.allowed_users_above_levels:
+        revoke_user_from_child_perimeters(perimeter=perimeter, user_id=user_id)
+
+
+def revoke_user_from_parent_perimeters(perimeter, user_id: int):
+    parent = perimeter.parent
+    for child in parent.children.all():
+        if user_id in child.allowed_users + child.allowed_users_inferior_levels:
+            return
+    try:
+        parent.allowed_users_inferior_levels.remove(user_id)
+        parent.save()
+    except ValueError:
+        pass
+    if parent.parent:
+        revoke_user_from_parent_perimeters(perimeter=parent, user_id=user_id)
+
+
+def revoke_user_from_child_perimeters(perimeter, user_id: int):
+    for child in perimeter.children.all():
+        try:
+            child.allowed_users_above_levels.remove(user_id)
+            child.save()
+        except ValueError:
+            pass
+        if child.children.all():
+            revoke_user_from_child_perimeters(perimeter=child, user_id=user_id)
