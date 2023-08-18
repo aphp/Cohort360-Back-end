4bcb9dd847ec26f5e8133304c9fa4c978b208dfb
hicham <hicham.taroq-ext@aphp.fr>
2023-08-18 17:40:19 +0200

    refactor: execute receiver work as async task

--- accesses/signals.py
+++ accesses/signals.py
@@ -1,10 +1,9 @@
-from itertools import chain
-
 from django.db.models.signals import post_save
 from django.dispatch import receiver
 from django.utils import timezone
 
-from accesses.models import Access, get_user_valid_manual_accesses, Perimeter
+from accesses.models import Access, get_user_valid_manual_accesses
+from accesses.tasks import add_new_allowed_user, revoke_user
 
 
 @receiver(signal=post_save, sender=Access, dispatch_uid="manage_onchange_allowed_users")
@@ -13,79 +12,14 @@ def manage_onchange_allowed_users(sender, **kwargs):
     is_new_access = kwargs.get("created")
     is_closed_or_invalid_access = not kwargs.get("created") and (access.end_datetime < timezone.now()
                                                                  or kwargs.get("invalid"))
-    perimeter = access.perimeter
+    perimeter_id = access.perimeter_id
     user_id = access.profile.user_id
 
     if is_new_access:
-        add_new_allowed_user(perimeter=perimeter, user_id=user_id)
-        add_user_to_parent_perimeters(perimeter=perimeter, user_id=user_id)
-        add_user_to_child_perimeters(perimeter=perimeter, user_id=user_id)
+        add_new_allowed_user.delay(perimeter_id, user_id)
     elif is_closed_or_invalid_access:
-        revoke_user(closed_access=access, perimeter=perimeter, user_id=user_id)
-
-
-def add_new_allowed_user(perimeter, user_id: int):
-    perimeter.allowed_users = list(set((perimeter.allowed_users or []) + [user_id]))
-    perimeter.save()
-
-
-def add_user_to_parent_perimeters(perimeter: Perimeter, user_id: int):
-    parent = perimeter.parent
-    if parent:
-        allowed_users_inferior_levels = parent.allowed_users_inferior_levels or []
-        parent.allowed_users_inferior_levels = list(set(allowed_users_inferior_levels + [user_id]))
-        parent.save()
-        if parent.parent:
-            add_user_to_parent_perimeters(perimeter=parent, user_id=user_id)
-
-
-def add_user_to_child_perimeters(perimeter: Perimeter, user_id: int):
-    for child in perimeter.children.all():
-        allowed_users_above_levels = child.allowed_users_above_levels or []
-        child.allowed_users_above_levels = list(set(allowed_users_above_levels + [user_id]))
-        child.save()
-        if child.children.all():
-            add_user_to_child_perimeters(perimeter=child, user_id=user_id)
-
-
-def revoke_user(closed_access: Access, perimeter: Perimeter, user_id: int):
-    extra_accesses_on_perimeter = get_user_valid_manual_accesses(user=closed_access.profile.user)\
-                                  .filter(perimeter_id=closed_access.perimeter_id)\
-                                  .exists()
-
-    if not extra_accesses_on_perimeter:
-        try:
-            perimeter.allowed_users.remove(user_id)
-            perimeter.save()
-        except ValueError:
-            return
-        revoke_user_from_parent_perimeters(perimeter=perimeter, user_id=user_id)
-        revoke_user_from_child_perimeters(perimeter=perimeter, user_id=user_id)
-
-
-def revoke_user_from_parent_perimeters(perimeter: Perimeter, user_id: int):
-    parent = perimeter.parent
-    for child in parent.children.all():
-        if user_id in chain(child.allowed_users,
-                            child.allowed_users_inferior_levels):
-            return
-    try:
-        parent.allowed_users_inferior_levels.remove(user_id)
-        parent.save()
-    except ValueError:
-        pass
-    if parent.parent:
-        revoke_user_from_parent_perimeters(perimeter=parent, user_id=user_id)
-
-
-def revoke_user_from_child_perimeters(perimeter: Perimeter, user_id: int):
-    if user_id not in chain(perimeter.allowed_users,
-                            perimeter.allowed_users_above_levels):
-        for child in perimeter.children.all():
-            try:
-                child.allowed_users_above_levels.remove(user_id)
-                child.save()
-            except ValueError:
-                pass
-            if child.children.all():
-                revoke_user_from_child_perimeters(perimeter=child, user_id=user_id)
+        extra_accesses_on_perimeter = get_user_valid_manual_accesses(user=access.profile.user) \
+                                      .filter(perimeter_id=access.perimeter_id) \
+                                      .exists()
+        if not extra_accesses_on_perimeter:
+            revoke_user.delay(perimeter_id, user_id)
--- accesses/tasks.py
+++ accesses/tasks.py
@@ -1,5 +1,10 @@
+from itertools import chain
+
+from celery import shared_task
+
 from accesses.accesses_alerts import send_access_expiry_alerts
 from accesses.conf_perimeters import perimeters_data_model_objects_update
+from accesses.models import Perimeter
 from admin_cohort import celery_app
 from admin_cohort.settings import ACCESS_EXPIRY_FIRST_ALERT_IN_DAYS, ACCESS_EXPIRY_SECOND_ALERT_IN_DAYS
 from admin_cohort.tools.celery_periodic_task_helper import ensure_single_task
@@ -16,3 +21,74 @@ def check_expiring_accesses():
 @ensure_single_task("perimeters_daily_update")
 def perimeters_daily_update():
     perimeters_data_model_objects_update()
+
+
+@shared_task
+def add_new_allowed_user(perimeter_id: int, user_id: int):
+    try:
+        perimeter = Perimeter.objects.get(id=perimeter_id)
+    except Perimeter.DoesNotExist:
+        return
+    perimeter.allowed_users = list(set((perimeter.allowed_users or []) + [user_id]))
+    perimeter.save()
+    add_user_to_parent_perimeters(perimeter=perimeter, user_id=user_id)
+    add_user_to_child_perimeters(perimeter=perimeter, user_id=user_id)
+
+
+@shared_task
+def revoke_user(perimeter_id: int, user_id: int):
+    try:
+        perimeter = Perimeter.objects.get(id=perimeter_id)
+        perimeter.allowed_users.remove(user_id)
+        perimeter.save()
+    except (Perimeter.DoesNotExist, ValueError):
+        return
+    revoke_user_from_parent_perimeters(perimeter=perimeter, user_id=user_id)
+    revoke_user_from_child_perimeters(perimeter=perimeter, user_id=user_id)
+
+
+def add_user_to_parent_perimeters(perimeter: Perimeter, user_id: int):
+    parent = perimeter.parent
+    if parent:
+        allowed_users_inferior_levels = parent.allowed_users_inferior_levels or []
+        parent.allowed_users_inferior_levels = list(set(allowed_users_inferior_levels + [user_id]))
+        parent.save()
+        if parent.parent:
+            add_user_to_parent_perimeters(perimeter=parent, user_id=user_id)
+
+
+def add_user_to_child_perimeters(perimeter: Perimeter, user_id: int):
+    for child in perimeter.children.all():
+        allowed_users_above_levels = child.allowed_users_above_levels or []
+        child.allowed_users_above_levels = list(set(allowed_users_above_levels + [user_id]))
+        child.save()
+        if child.children.all():
+            add_user_to_child_perimeters(perimeter=child, user_id=user_id)
+
+
+def revoke_user_from_parent_perimeters(perimeter: Perimeter, user_id: int):
+    parent = perimeter.parent
+    for child in parent.children.all():
+        if user_id in chain(child.allowed_users,
+                            child.allowed_users_inferior_levels):
+            return
+    try:
+        parent.allowed_users_inferior_levels.remove(user_id)
+        parent.save()
+    except ValueError:
+        pass
+    if parent.parent:
+        revoke_user_from_parent_perimeters(perimeter=parent, user_id=user_id)
+
+
+def revoke_user_from_child_perimeters(perimeter: Perimeter, user_id: int):
+    if user_id not in chain(perimeter.allowed_users,
+                            perimeter.allowed_users_above_levels):
+        for child in perimeter.children.all():
+            try:
+                child.allowed_users_above_levels.remove(user_id)
+                child.save()
+            except ValueError:
+                pass
+            if child.children.all():
+                revoke_user_from_child_perimeters(perimeter=child, user_id=user_id)
