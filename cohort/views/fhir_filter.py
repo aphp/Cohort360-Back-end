25c0b27cdec177ae782133985f709defdc8e19bd
anddy <anddy.seruga@aphp.fr>
2023-08-21 11:41:57 +0200

    refactor: move filter logic to a filter class

--- cohort/views/fhir_filter.py
+++ cohort/views/fhir_filter.py
@@ -1,27 +1,28 @@
+from django_filters import rest_framework as filters
 from rest_framework import viewsets
-from rest_framework.decorators import action
-from rest_framework.filters import SearchFilter
 from rest_framework.pagination import LimitOffsetPagination
-from rest_framework.response import Response
 
 from admin_cohort.tools.cache import cache_response
 from cohort.models import FhirFilter
 from cohort.serializers import FhirFilterSerializer
 
 
+class FhirFilterFilter(filters.FilterSet):
+    ordering = filters.OrderingFilter(fields=('-created_at', 'modified_at'))
+
+    class Meta:
+        model = FhirFilter
+        fields = ('fhir_resource', 'name', 'owner', 'created_at', 'modified_at')
+
+
 class FhirFilterViewSet(viewsets.ModelViewSet):
     queryset = FhirFilter.objects.all()
     serializer_class = FhirFilterSerializer
     pagination_class = LimitOffsetPagination
+    filterset_class = FhirFilterFilter
+    lookup_field = "pk"  # Change this line to use "pk" as the lookup field
     swagger_tags = ["Cohort - fhir_filter"]
-    filter_backends = [SearchFilter]
-    search_fields = ("filter_name", 'fhir_resource')
-
-    @action(detail=False, methods=['GET'])
-    def recent_filters(self, request):
-        filters = FhirFilter.objects.filter(owner=request.user).order_by('-created_at')
-        serializer = FhirFilterSerializer(filters, many=True)
-        return Response(serializer.data)
+    http_method_names = ["get", "post", "patch"]
 
     @cache_response()
     def list(self, request, *args, **kwargs):
