2e080f5845b7fa5a53728c5fe9959d704b396171
anddy <anddy.seruga@aphp.fr>
2023-08-30 12:10:42 +0200

    feat: add crb class handling previous java CRB logic

---
+++ cohort/crb/crb.py
@@ -0,0 +1,10 @@
+from cohort.crb.cohort_requests.abstract_cohort_request import AbstractCohortRequest
+from cohort.crb.fhir_request import FhirRequest
+
+
+class CRB:
+    def __init__(self, cohort_action: AbstractCohortRequest):
+        self.cohort_action = cohort_action
+
+    def request_to_sjs(self, fhir_request: FhirRequest) -> str:
+        return self.cohort_action.action(fhir_request)
