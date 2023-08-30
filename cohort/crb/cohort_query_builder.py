7ac22c79a889266fa1bfeb41b9209ed91c17cb11
anddy <anddy.seruga@aphp.fr>
2023-08-30 11:57:31 +0200

    feat: create cohort query builder class

---
+++ cohort/crb/cohort_query_builder.py
@@ -0,0 +1,19 @@
+from dataclasses import dataclass
+
+from cohort.crb.enums import Mode
+from cohort.crb.fhir_request import FhirRequest
+from cohort.crb.format_query import FormatQuery
+from cohort.crb.spark_job_object import SparkJobObject
+
+
+@dataclass
+class CohortQueryBuilder:
+    username: str
+    format_query: FormatQuery = FormatQuery()
+
+    def create_request(self, fhir_request: FhirRequest, mode: Mode) -> SparkJobObject:
+        if fhir_request is None:
+            raise ...
+        sjs_request = self.format_query.format_to_fhir(fhir_request)
+        fhir_request.request = sjs_request
+        return SparkJobObject("Created from Django", fhir_request, mode, self.username)
