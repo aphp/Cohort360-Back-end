168b88381e5d234f919f76226e4c64b6342ef3ec
anddy <anddy.seruga@aphp.fr>
2023-08-30 12:07:39 +0200

    feat: create class to call SJS with the create method

---
+++ cohort/crb/cohort_requests/create.py
@@ -0,0 +1,12 @@
+from cohort.crb.cohort_requests.abstract_cohort_request import AbstractCohortRequest
+from cohort.crb.enums import Mode
+from cohort.crb.fhir_request import FhirRequest
+
+
+class CohortCreate(AbstractCohortRequest):
+    def __init__(self, *args, **kwargs):
+        super().__init__(mode=Mode.CREATE, *args, **kwargs)
+
+    def action(self, fhir_request: FhirRequest) -> str:
+        request = self.create_request(fhir_request)
+        return self.sjs_client.create(request)
