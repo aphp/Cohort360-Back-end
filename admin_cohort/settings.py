a57e40eec15120a27b7da45bc55f1b2c1da55faa
anddy <anddy.seruga@aphp.fr>
2023-08-30 12:22:41 +0200

    feat: add sjs url in settings

--- admin_cohort/settings.py
+++ admin_cohort/settings.py
@@ -18,6 +18,7 @@ BACK_HOST = env("BACK_HOST")
 BACK_URL = f"https://{env('BACK_HOST')}"
 FRONT_URL = env("FRONT_URL")
 FRONT_URLS = env("FRONT_URLS").split(',')
+SJS_URL = env('SJS_URL')
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = env("DJANGO_SECRET_KEY")
