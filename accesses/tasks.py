250b97cf08a432f7dc14c70247fd8c67357d1833
Thicham <taroqhicham@gmail.com>
2023-09-12 14:48:13 +0200

    fix: count users on perimeters (#268)
    Co-authored-by: hicham <hicham.taroq-ext@aphp.fr>

--- accesses/tasks.py
+++ accesses/tasks.py
@@ -33,10 +33,9 @@ def count_users_on_perimeters():
 
 def count_allowed_users():
     perimeters_ids = Access.objects.filter(q_is_valid_access())\
-                                   .distinct("profile__user_id") \
+                                   .distinct("perimeter_id", "profile__user_id") \
                                    .values_list("perimeter_id", flat=True)
     counter = Counter(perimeters_ids)
-
     for perimeter_id, count in counter.items():
         try:
             perimeter = Perimeter.objects.get(pk=perimeter_id)
