4787bc5f8a91e0db801c8a01cd75a92065a72815
anddy <anddy.seruga@aphp.fr>
2023-09-13 10:26:38 +0200

    Add count, create and delete for sjs client

--- cohort/crb/sjs_client.py
+++ cohort/crb/sjs_client.py
@@ -1,6 +1,3 @@
-import json
-from dataclasses import asdict
-
 import requests
 
 from admin_cohort.settings import SJS_URL
@@ -8,24 +5,43 @@ from cohort.crb.spark_job_object import SparkJobObject
 
 
 class SjsClient:
-    app_name = "omop-spark-job"
+    APP_NAME = "omop-spark-job"
     url = SJS_URL
 
     # todo: remove this dependency by refactoring the SJS scala project and removing those class paths requirements
-    count_classpath = "fr.aphp.id.eds.requester.CountQuery"
-    create_classpath = "fr.aphp.id.eds.requester.CreateQuery"
+    COUNT_CLASSPATH = "fr.aphp.id.eds.requester.CountQuery"
+    CREATE_CLASSPATH = "fr.aphp.id.eds.requester.CreateQuery"
+    CONTEXT = "shared"
 
-    def count(self, request: str):
-        resp = requests.post(url=f"{self.url}/jobs",
-                             params={'appName': self.app_name},
-                             json=request
-                             )
+    def count(self, request):
+        params = {
+            'appName': self.APP_NAME,
+            'classPath': self.COUNT_CLASSPATH,
+            'context': self.CONTEXT
+        }
+
+        resp = requests.post(f"{self.url}/jobs", params=params, json=request)
         resp.raise_for_status()
         result = resp.json()
         return resp, result
 
     def create(self, request):
-        ...
+        params = {
+            'appName': self.APP_NAME,
+            'classPath': self.CREATE_CLASSPATH,
+            'context': self.CONTEXT,
+            'sync': 'false'
+        }
+
+        resp = requests.post(f"{self.url}/jobs", params=params, json=request)
+        resp.raise_for_status()
+        result = resp.json()
+        return resp, result
+
+    def delete(self, job_id: str) -> str:
+        resp = requests.delete(f"{self.url}/jobs/{job_id}")
+        resp.raise_for_status()
+        return resp.text
 
 
 def format_spark_job_request_for_sjs(spark_job_request: SparkJobObject) -> str:
