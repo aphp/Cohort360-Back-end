9cc0d223f48509c7f58814db3d09ca774e146a3f
anddy <anddy.seruga@aphp.fr>
2023-09-25 14:01:32 +0200

    feat: add source population in CohortCountAll fields

--- cohort/crb/cohort_requests/count_all.py
+++ cohort/crb/cohort_requests/count_all.py
@@ -1,22 +1,15 @@
-from accesses.conf_perimeters import CareSite
 from cohort.crb.cohort_requests.abstract_cohort_request import AbstractCohortRequest
 from cohort.crb.enums import Mode
 from cohort.crb.fhir_request import FhirRequest
 from cohort.crb.source_population import SourcePopulation
 
 
-def fetch_aphp_care_site_id() -> int:
-    care_site: CareSite = CareSite.objects.first()
-    return care_site.cohort_id
-
-
 class CohortCountAll(AbstractCohortRequest):
-    def __init__(self, *args, **kwargs):
+    def __init__(self, source_population: SourcePopulation, *args, **kwargs):
         super().__init__(mode=Mode.COUNT_ALL, *args, **kwargs)
+        self.source_population = source_population
 
     def action(self, fhir_request: FhirRequest) -> str:
-        cohort_id = fetch_aphp_care_site_id()
-        print(cohort_id)
-        fhir_request.source_population = SourcePopulation(care_site_cohort_list=[cohort_id])
+        fhir_request.source_population = self.source_population
         request = self.create_request(fhir_request)
         return self.sjs_client.count(request)
