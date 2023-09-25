b8f7f14df29702e2040bf275c71137ddb8865a3c
anddy <anddy.seruga@aphp.fr>
2023-09-25 13:53:28 +0200

    feat: make FhirRequest a pydantic basemodel

--- cohort/crb/fhir_request.py
+++ cohort/crb/fhir_request.py
@@ -1,7 +1,12 @@
 from dataclasses import field, dataclass
+from dataclasses import field
 
+from pydantic import BaseModel, Field
+
+from cohort.crb.criteria import Criteria
 from cohort.crb.enums import CriteriaType
 from cohort.crb.ranges import TemporalConstraint, Criteria
+from cohort.crb.ranges import TemporalConstraint
 from cohort.crb.source_population import SourcePopulation
 
 
@@ -13,3 +18,11 @@ class FhirRequest:
     type: CriteriaType | None = None
     request: Criteria | None = None
     temporal_constraints: list[TemporalConstraint] = field(default_factory=list)
+class FhirRequest(BaseModel):
+    cohort_uuid: str = Field(None)
+    cohort_name: str = Field(None)
+    source_population: SourcePopulation = Field(alias="sourcePopulation")
+    criteria_type: CriteriaType = Field(None, alias="_type")
+    request: Criteria = Field(None)
+    temporal_constraints: list[TemporalConstraint] = Field(default_factory=list)
+
