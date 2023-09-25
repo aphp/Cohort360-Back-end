49cd7118ed3eb192047da30b078b70c9a823de85
hicham <hicham.taroq-ext@aphp.fr>
2023-09-25 10:38:30 +0200

    fix: attach logo to email signature

--- admin_cohort/emails.py
+++ admin_cohort/emails.py
@@ -1,12 +1,13 @@
 import logging
 from dataclasses import dataclass
+from email.mime.image import MIMEImage
 from smtplib import SMTPException
 from typing import Optional
 
 from django.core.mail import EmailMultiAlternatives
 from django.template.loader import get_template
 
-from admin_cohort.settings import EMAIL_SENDER_ADDRESS
+from admin_cohort.settings import EMAIL_SENDER_ADDRESS, STATIC_ROOT
 
 _logger = logging.getLogger('django.request')
 
@@ -26,6 +27,16 @@ class EmailNotification:
         self.txt_content = get_template(self.txt_template).render(self.context)
 
     @staticmethod
+    def attach_logo(email):
+        email.mixed_subtype = 'related'
+        logo = 'logo_cohort360.png'
+        logo_path = STATIC_ROOT/"admin_cohort"/"img"/logo
+        with open(logo_path, 'rb') as f:
+            img = MIMEImage(f.read())
+            img.add_header('Content-ID', f'<{logo}>')
+        email.attach(img)
+
+    @staticmethod
     def send_email(email) -> None:
         email.send()
 
@@ -36,6 +47,7 @@ class EmailNotification:
                                        from_email=EMAIL_SENDER_ADDRESS,
                                        to=[self.to])
         email.attach_alternative(content=self.html_content, mimetype="text/html")
+        self.attach_logo(email=email)
         try:
             self.send_email(email)
         except (SMTPException, TimeoutError):
