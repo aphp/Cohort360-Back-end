bd5d34de935eb2a02d0f113a70d1d3cad837b504
anddy <anddy.seruga@aphp.fr>
2023-09-26 10:15:20 +0200

    refactor: rename FhirRequest -> CohortQuery

--- cohort/crb/cohort_query.py
+++ cohort/crb/cohort_query.py
@@ -1,28 +1,15 @@
-from dataclasses import field, dataclass
-from dataclasses import field
-
 from pydantic import BaseModel, Field
 
 from cohort.crb.criteria import Criteria
 from cohort.crb.enums import CriteriaType
-from cohort.crb.ranges import TemporalConstraint, Criteria
 from cohort.crb.ranges import TemporalConstraint
 from cohort.crb.source_population import SourcePopulation
 
 
-@dataclass
-class FhirRequest:
-    cohort_uuid: str | None = None
-    cohort_name: str | None = None
-    source_population: SourcePopulation | None = None
-    type: CriteriaType | None = None
-    request: Criteria | None = None
-    temporal_constraints: list[TemporalConstraint] = field(default_factory=list)
-class FhirRequest(BaseModel):
+class CohortQuery(BaseModel):
     cohort_uuid: str = Field(None)
     cohort_name: str = Field(None)
     source_population: SourcePopulation = Field(alias="sourcePopulation")
     criteria_type: CriteriaType = Field(None, alias="_type")
     request: Criteria = Field(None)
     temporal_constraints: list[TemporalConstraint] = Field(default_factory=list)
-
--- cohort/crb/cohort_requests/count.py
+++ cohort/crb/cohort_requests/count.py
@@ -1,12 +1,12 @@
 from cohort.crb.cohort_requests.abstract_cohort_request import AbstractCohortRequest
 from cohort.crb.enums import Mode
-from cohort.crb.fhir_request import FhirRequest
+from cohort.crb.cohort_query import CohortQuery
 
 
 class CohortCount(AbstractCohortRequest):
     def __init__(self, *args, **kwargs):
         super().__init__(mode=Mode.COUNT, *args, **kwargs)
 
-    def action(self, fhir_request: FhirRequest) -> str:
+    def action(self, fhir_request: CohortQuery) -> str:
         request = self.create_request(fhir_request)
         return self.sjs_client.count(request)
