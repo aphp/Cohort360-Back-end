6b5e33b32d977885ffb5f4c8591f5944f369cc24
anddy <anddy.seruga@aphp.fr>
2023-09-29 15:34:30 +0200

    refactor: dynamically add the top caresite to the query

--- cohort/crb/cohort_requests/count_all.py
+++ cohort/crb/cohort_requests/count_all.py
@@ -1,23 +1,30 @@
 from __future__ import annotations
 
+import os
 from typing import TYPE_CHECKING
 
+from requests import Response
+
+from accesses.models import Perimeter
+from cohort.crb import SourcePopulation
 from cohort.crb.cohort_requests.abstract_cohort_request import AbstractCohortRequest
 from cohort.crb.enums import Mode
 
 if TYPE_CHECKING:
-    from cohort.crb import CohortQuery, SourcePopulation
+    from cohort.crb import CohortQuery
+
+env = os.environ
 
 
 class CohortCountAll(AbstractCohortRequest):
-    def __init__(self, source_population: SourcePopulation, *args, **kwargs):
+    def __init__(self, *args, **kwargs):
         super().__init__(mode=Mode.COUNT_ALL, *args, **kwargs)
-        self.source_population = source_population
 
-    def action(self, cohort_query: CohortQuery) -> str:
-        cohort_query.source_population = self.source_population
+    def action(self, cohort_query: CohortQuery) -> tuple[Response, dict]:
+        cohort_query.source_population = SourcePopulation(caresiteCohortList=[get_top_care_site_source_population()])
         request = self.create_request_for_sjs(cohort_query)
         return self.sjs_client.count(request)
+
 
 def get_top_care_site_source_population() -> int:
     return Perimeter.objects.get(level=1).cohort_id
