15ed4defcf7d84cbbbc5de5bc139a3c0a93e8361
anddy <anddy.seruga@aphp.fr>
2023-10-03 10:26:02 +0200

    feat: get the owner entity id dynamically from headers

--- cohort/crb/cohort_requests/abstract_cohort_request.py
+++ cohort/crb/cohort_requests/abstract_cohort_request.py
@@ -3,6 +3,7 @@ from __future__ import annotations
 from abc import ABC, abstractmethod
 from typing import TYPE_CHECKING
 
+from admin_cohort.auth.utils import get_userinfo_from_token
 from cohort.crb.enums import Mode
 from cohort.crb.exceptions import FhirException
 from cohort.crb.query_formatter import QueryFormatter
@@ -19,15 +20,25 @@ class AbstractCohortRequest(ABC):
         self.sjs_client = sjs_client
         self.auth_headers = auth_headers
 
+    def __headers_to_owner_entity(self) -> str:
+        user = get_userinfo_from_token(
+            self.auth_headers['Authorization'].replace('Bearer ', ''),
+            self.auth_headers['authorizationMethod']
+        )
+        return user.username
+
     def create_request_for_sjs(self, cohort_query: CohortQuery) -> str:
         """Format the given query with the Fhir nomenclature and return a dict to be sent
         for the followup sjs request."""
         if cohort_query is None:
             raise FhirException("No query received to format.")
-        format_query = QueryFormatter(self.auth_headers)
-        sjs_request = format_query.format_to_fhir(cohort_query)
+
+        sjs_request = QueryFormatter(self.auth_headers).format_to_fhir(cohort_query)
         cohort_query.criteria = sjs_request
-        spark_job_request = SparkJobObject("Created from Django", cohort_query, self.mode, cohort_query.cohort_uuid)
+
+        spark_job_request = SparkJobObject(
+            "Created from Django", cohort_query, self.mode, self.__headers_to_owner_entity()
+        )
         return format_spark_job_request_for_sjs(spark_job_request)
 
     @abstractmethod
