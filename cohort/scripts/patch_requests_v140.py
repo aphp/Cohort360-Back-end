8ead2cfcbc918f1c6ab596edee8836712aade7d5
Paul Bui-Quang <paul.bui-quang@aphp.fr>
2023-10-11 15:30:44 +0200

    fix(serializedquery): correct field mapping + add medication new mapping (#281)

--- cohort/scripts/patch_requests_v140.py
+++ cohort/scripts/patch_requests_v140.py
@@ -1,39 +1,47 @@
-from cohort.scripts.query_request_updater import QueryRequestUpdater, RESOURCE_DEFAULT
+import logging
+import re
+
+from django.db import connections
+
+from cohort.scripts.query_request_updater import QueryRequestUpdater, RESOURCE_DEFAULT, MATCH_ALL_VALUES
+
+LOGGER = logging.getLogger("info")
 
 NEW_VERSION = "v1.4.0"
 
 FILTER_MAPPING = {
     "Encounter": {
-        "_has:Patient:encounter:active": "subject.active"
+        "_has:Patient:encounter:active": "subject.active",
+        "service-provider": "encounter-care-site"
     },
     "DocumentReference": {
         "patient-active": "subject.active"
     },
     "MedicationAdministration": {
-        "hierarchy-ATC": "medication-hierarchy",
+        "hierarchy-ATC": "medication",
         "medication-simple": "medication",
         "patient-active": "subject.active"
     },
     "MedicationRequest": {
-        "hierarchy-ATC": "medication-hierarchy",
+        "hierarchy-ATC": "medication",
         "medication-simple": "medication",
         "patient-active": "subject.active"
     },
     "Claim": {
         "code": "diagnosis",
-        "codeList": "diagnosis-hierarchy",
+        "codeList": "diagnosis",
         "patient-active": "patient.active"
     },
     "Procedure": {
-        "codeList": "code-hierarchy",
+        "codeList": "code",
         "patient-active": "subject.active"
     },
     "Condition": {
-        "codeList": "code-hierarchy",
+        "codeList": "code",
         "patient-active": "subject.active"
     },
     "Observation": {
-        "part-of": "code-hierarchy",
+        "part-of": "code",
         "value-quantity-value": "value-quantity",
         "row_status": "status",
         "encounter-service-provider": "encounter.encounter-care-site",
@@ -47,17 +55,73 @@ FILTER_NAME_TO_SKIP = {
     "DocumentReference": ["empty"]
 }
 
+UCD_ORBIS_CODESYSTEM = "https://terminology.eds.aphp.fr/aphp-orbis-medicament-code-ucd"
+ATC_ORBIS_CODESYSTEM = "https://terminology.eds.aphp.fr/aphp-orbis-medicament-atc-article"
+ATC_CODEYSTEM = "https://terminology.eds.aphp.fr/atc"
+
+code_mapping_cache = {
+}
+
+
+def find_related_atc(code: str):
+    if code in code_mapping_cache:
+        return code
+    LOGGER.info(f"Searching for code {code}")
+    cursor = connections["omop"].cursor()
+    cursor.execute(f'''
+    WITH orbis AS (
+        SELECT source_concept_id as orbis_atc_id,source_concept_code as orbis_atc_code 
+        FROM omop.concept_fhir 
+        WHERE source_vocabulary_reference = '{ATC_ORBIS_CODESYSTEM}' AND delete_datetime IS NULL
+        ),
+        atc AS (
+        SELECT source_concept_id as atc_id,source_concept_code as atc_code 
+        FROM omop.concept_fhir 
+        WHERE source_vocabulary_reference = '{ATC_CODEYSTEM}' AND delete_datetime IS NULL
+        )
+    SELECT atc_code FROM omop.concept_relationship r
+    INNER JOIN orbis o
+    ON o.orbis_atc_id = r.concept_id_1
+    INNER JOIN atc a
+    ON a.atc_id = r.concept_id_2
+    WHERE relationship_id = 'Maps to' AND r.delete_datetime IS NULL AND o.orbis_atc_code = '{code}';
+''')
+    res = cursor.fetchone()
+    if not res:
+        LOGGER.info(f"Failed to find related atc code {code}")
+        if re.match("\\d+", code):
+            return UCD_ORBIS_CODESYSTEM + "|" + code
+        return ATC_ORBIS_CODESYSTEM + "|" + code
+    code_mapping_cache[code] = ATC_CODEYSTEM + "|" + res[0]
+    return code_mapping_cache[code]
+
+
+def find_related_atc_codes(codes: str):
+    LOGGER.info(f"Translating codes {codes}")
+    return ",".join([find_related_atc(code) for code in codes.split(",")])
+
+
 FILTER_VALUE_MAPPING = {
     "Observation": {
         "row_status": {
             "Validé": "Val"
+        }
+    },
+    "MedicationRequest": {
+        "hierarchy-ATC": {
+            MATCH_ALL_VALUES: find_related_atc_codes
+        }
+    },
+    "MedicationAdministration": {
+        "hierarchy-ATC": {
+            MATCH_ALL_VALUES: find_related_atc_codes
         }
     }
 }
 
 STATIC_REQUIRED_FILTERS = {
     "DocumentReference": [
-        "contenttype=http%3A%2F%2Fterminology.hl7.org%2FCodeSystem%2Fv3-mediatypes%7Ctext%2Fplain"
+        "contenttype=http://terminology.hl7.org/CodeSystem/v3-mediatypes|text/plain"
     ]
 }
 
@@ -72,3 +136,17 @@ updater_v140 = QueryRequestUpdater(
     static_required_filters=STATIC_REQUIRED_FILTERS,
     resource_name_mapping=RESOURCE_NAME_MAPPING
 )
+
+
+#
+# @dataclasses.dataclass
+# class Dummy:
+#     serialized_query: str
+#
+#
+# def exec():
+#     import json
+#     with open("/tmp/cohort_requestquerysnapshot.json", "r") as fh:
+#         queries = [Dummy(q["serialized_query"]) for q in json.load(fh)]
+#
+#     updater_v140.do_update_old_query_snapshots(queries, None, dry_run=True, debug=True)
