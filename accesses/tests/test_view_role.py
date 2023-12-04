7196e3e931b809b8503add35bd8cc165d51b968d
hicham <hicham.taroq-ext@aphp.fr>
2023-12-04 09:47:11 +0100

    refactor: tests ongoing WIP

--- accesses/tests/test_view_role.py
+++ accesses/tests/test_view_role.py
@@ -44,7 +44,7 @@ role_roles_manager_data = {**ALL_FALSY_RIGHTS,
 # todo: suggestion: remove right_manage/read_roles
 #       only full_admin can manage roles on Portail
 #       make roles available for all users to see on Portail
-
+6+
 
 class RoleRetrieveFilter(CaseRetrieveFilter):
 
