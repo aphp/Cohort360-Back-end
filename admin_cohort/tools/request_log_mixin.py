7f09e1ac31158bff5b5c6fa59caaea1fd3252aea
hicham <hicham.taroq-ext@aphp.fr>
2023-12-15 10:11:00 +0100

    fix: log request data and params

--- admin_cohort/tools/request_log_mixin.py
+++ admin_cohort/tools/request_log_mixin.py
@@ -9,7 +9,7 @@ _logger = logging.getLogger("django.request")
 
 class RequestLogMixin(LoggingMixin):
     def handle_log(self):
-        for f in ['query_params', 'data', 'errors', 'response']:
+        for f in ['errors', 'response']:
             self.log.pop(f, None)
         return super(RequestLogMixin, self).handle_log()
 
