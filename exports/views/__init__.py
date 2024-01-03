f7c0fef34e444319469409d58c68a59afbfdef68
Thicham <taroqhicham@gmail.com>
2024-01-03 10:45:42 +0100

    feat(fhir filters): get filters by user id and resource (#307)

--- exports/views/__init__.py
+++ exports/views/__init__.py
@@ -1,9 +1,11 @@
 from .cohort import CohortViewSet
 from .export_request import ExportRequestViewSet
+from .fhir_filter import FhirFilterViewSet
 from exports.views.v1 import ExportViewSet, ExportTableViewSet, ExportResultStatViewSet, DatalabViewSet, InfrastructureProviderViewSet
 
 __all__ = ["ExportRequestViewSet",
            "CohortViewSet",
+           "FhirFilterViewSet",
            "ExportViewSet",
            "ExportTableViewSet",
            "ExportResultStatViewSet",
