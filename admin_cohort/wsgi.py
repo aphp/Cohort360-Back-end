af85d210aa2d86ad9460b3f2cf016eba6e0fe086
anddy <anddy.seruga@aphp.fr>
2024-02-07 10:49:04 +0100

    refactor: revert wsgi changes since asgi is created

--- admin_cohort/wsgi.py
+++ admin_cohort/wsgi.py
@@ -9,23 +9,8 @@ https://docs.djangoproject.com/en/2.2/howto/deployment/wsgi/
 
 import os
 
-from channels.auth import AuthMiddlewareStack
-from channels.routing import ProtocolTypeRouter, URLRouter
 from django.core.wsgi import get_wsgi_application
-
-from admin_cohort import websocket_consumer
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'admin_cohort.settings')
 
-# application = get_wsgi_application()
-
-application = ProtocolTypeRouter(
-    {
-        "http": get_wsgi_application(),
-        "websocket": AuthMiddlewareStack(
-            URLRouter(
-                websocket_consumer
-            )
-        ),
-    }
-)
+application = get_wsgi_application()
