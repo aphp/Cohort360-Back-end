0f3b0716da3a2139d5587418107c25e8a2cbc551
Hicham <hicham.taroq-ext@aphp.fr>
2024-03-06 14:13:54 +0100

    fix: activate virtual env

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -7,6 +7,7 @@ sed -i s/{{BACK_HOST}}/"$BACK_HOST"/g /etc/nginx/nginx.conf;
 
 service nginx restart
 
+source venv/bin/activate
 python manage.py migrate --database="default"
 python manage.py collectstatic --noinput
 
