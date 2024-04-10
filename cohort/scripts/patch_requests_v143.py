1b048d7764965d4586c91dd2adac332705a23434
Paul Bui-Quang <paul.bui-quang@aphp.fr>
2024-04-10 11:47:32 +0200

    feat(scriptMigration): add new migration script for Condition new codesystem

---
+++ cohort/scripts/patch_requests_v143.py
@@ -0,0 +1,63 @@
+import logging
+
+from cohort.scripts.patch_requests_v141 import NEW_VERSION as PREV_VERSION_2
+from cohort.scripts.patch_requests_v142 import NEW_VERSION as PREV_VERSION_3
+from cohort.scripts.patch_requests_v140 import NEW_VERSION as PREV_VERSION, find_related_atc as find_related_atc_v140
+from cohort.scripts.query_request_updater import RESOURCE_DEFAULT, MATCH_ALL_VALUES, QueryRequestUpdater
+
+LOGGER = logging.getLogger("info")
+
+NEW_VERSION = "v1.4.3"
+
+FILTER_MAPPING = {
+    RESOURCE_DEFAULT: {
+    }
+}
+
+FILTER_NAME_TO_SKIP = {
+}
+
+code_mapping_cache = {
+}
+
+
+def find_related_atc(code: str):
+    if code.startswith("undefined|"):
+        code_value = code.split("|")
+        return find_related_atc_v140(code_value[1])
+    return code
+
+
+def find_related_atc_codes(codes: str):
+    LOGGER.info(f"Translating codes {codes}")
+    return ",".join([find_related_atc(code) for code in codes.split(",")])
+
+
+FILTER_VALUE_MAPPING = {
+    "MedicationRequest": {
+        "medication": {
+            MATCH_ALL_VALUES: find_related_atc_codes
+        }
+    },
+    "MedicationAdministration": {
+        "medication": {
+            MATCH_ALL_VALUES: find_related_atc_codes
+        }
+    }
+}
+
+STATIC_REQUIRED_FILTERS = {
+}
+
+RESOURCE_NAME_MAPPING = {
+}
+
+updater_v143 = QueryRequestUpdater(
+    version_name=NEW_VERSION,
+    previous_version_name=[PREV_VERSION, PREV_VERSION_2, PREV_VERSION_3],
+    filter_mapping=FILTER_MAPPING,
+    filter_names_to_skip=FILTER_NAME_TO_SKIP,
+    filter_values_mapping=FILTER_VALUE_MAPPING,
+    static_required_filters=STATIC_REQUIRED_FILTERS,
+    resource_name_mapping=RESOURCE_NAME_MAPPING
+)
