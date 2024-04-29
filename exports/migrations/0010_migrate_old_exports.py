00029efb169a27abc04654eaec1d689c8cf5c87b
Hicham <hicham.taroq-ext@aphp.fr>
2024-04-29 13:37:13 +0200

    fix(exports): include exported tables in migration

--- exports/migrations/0010_migrate_old_exports.py
+++ exports/migrations/0010_migrate_old_exports.py
@@ -6,9 +6,11 @@ from django.db import migrations
 _logger = logging.getLogger('info')
 
 
-def fill_exports_table_with_export_requests(apps, schema_editor):
+def populate_exports_with_export_requests(apps, schema_editor):
     export_request_model = apps.get_model('exports', 'ExportRequest')
+    export_request_table_model = apps.get_model('exports', 'ExportRequestTable')
     export_model = apps.get_model('exports', 'Export')
+    export_table_model = apps.get_model('exports', 'ExportTable')
     datalab_model = apps.get_model('exports', 'Datalab')
     db_alias = schema_editor.connection.alias
 
@@ -17,26 +19,30 @@ def fill_exports_table_with_export_requests(apps, schema_editor):
         datalab = None
         if er.target_unix_account:
             datalab = datalab_model.objects.using(db_alias).get(name=er.target_unix_account.name)
-        export_model.objects.using(db_alias).create(data_exporter_version='3',
-                                                    data_version='NA',
-                                                    deleted=er.delete_datetime,
-                                                    deleted_by_cascade=False,
-                                                    created_at=er.insert_datetime,
-                                                    modified_at=er.update_datetime,
-                                                    clean_datetime=er.cleaned_at,
-                                                    motivation=er.motivation,
-                                                    output_format=er.output_format,
-                                                    target_name=er.target_name,
-                                                    target_location=er.target_location,
-                                                    nominative=er.nominative,
-                                                    shift_dates=er.shift_dates,
-                                                    is_user_notified=er.is_user_notified,
-                                                    owner_id=er.owner_id,
-                                                    datalab=datalab,
-                                                    request_job_duration=er.request_job_duration,
-                                                    request_job_id=er.request_job_id,
-                                                    request_job_status=er.request_job_status,
-                                                    request_job_fail_msg=er.request_job_fail_msg)
+        export = export_model.objects.using(db_alias).create(data_exporter_version='3',
+                                                             data_version='NA',
+                                                             deleted=er.delete_datetime,
+                                                             deleted_by_cascade=False,
+                                                             created_at=er.insert_datetime,
+                                                             modified_at=er.update_datetime,
+                                                             clean_datetime=er.cleaned_at,
+                                                             motivation=er.motivation,
+                                                             output_format=er.output_format,
+                                                             target_name=er.target_name,
+                                                             target_location=er.target_location,
+                                                             nominative=er.nominative,
+                                                             shift_dates=er.shift_dates,
+                                                             is_user_notified=er.is_user_notified,
+                                                             owner_id=er.owner_id,
+                                                             datalab=datalab,
+                                                             request_job_duration=er.request_job_duration,
+                                                             request_job_id=er.request_job_id,
+                                                             request_job_status=er.request_job_status,
+                                                             request_job_fail_msg=er.request_job_fail_msg)
+        for table in er.tables.all():
+            export_table_model.objects.using(db_alias).create(export=export,
+                                                              name=table.omop_table_name,
+                                                              respect_table_relationships=True)
         count += 1
     _logger.info(f'Populated {count} exports from old records')
 
@@ -48,5 +54,5 @@ class Migration(migrations.Migration):
     ]
 
     operations = [
-        migrations.RunPython(code=fill_exports_table_with_export_requests, reverse_code=migrations.RunPython.noop),
+        migrations.RunPython(code=populate_exports_with_export_requests, reverse_code=migrations.RunPython.noop),
     ]
