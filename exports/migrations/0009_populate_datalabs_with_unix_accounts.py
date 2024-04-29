b7792bb9a9e71b9370a43aab6e7002240b3874f8
Hicham <hicham.taroq-ext@aphp.fr>
2024-04-29 10:47:59 +0200

    fix(exports): migration to populate datalabs
    (cherry picked from commit 0513a372fdd0f06adbfe9718522e9c886562db16)

--- exports/migrations/0009_populate_datalabs_with_unix_accounts.py
+++ exports/migrations/0009_populate_datalabs_with_unix_accounts.py
@@ -1,9 +1,28 @@
 # written by HT on 2024-04-26 17:02
+import logging
 
-from django.db import migrations, models
+from django.db import migrations
 
 
-populate_datalabs = "INSERT INTO datalab (name) SELECT username FROM workspaces_account;"
+_logger = logging.getLogger('info')
+
+
+def populate_datalabs(apps, schema_editor):
+    infrastructure_provider_model = apps.get_model('exports', 'InfrastructureProvider')
+    datalab_model = apps.get_model('exports', 'Datalab')
+    account_model = apps.get_model('workspaces', 'Account')
+    db_alias = schema_editor.connection.alias
+
+    infra_provider = infrastructure_provider_model.objects.using(db_alias).first()
+    if not infra_provider:
+        infra_provider = infrastructure_provider_model.objects.using(db_alias).create(name='Main')
+        
+    count = 0
+    for account_name in account_model.objects.using(db_alias).all().values_list('name', flat=True):
+        datalab_model.objects.using(db_alias).create(name=account_name,
+                                                     infrastructure_provider=infra_provider)
+        count += 1
+    _logger.info(f'Populated {count} datalab accounts')
 
 
 class Migration(migrations.Migration):
@@ -13,5 +32,5 @@ class Migration(migrations.Migration):
     ]
 
     operations = [
-        migrations.RunSQL(sql=populate_datalabs, reverse_sql=migrations.RunSQL.noop),
+        migrations.RunPython(code=populate_datalabs, reverse_code=migrations.RunPython.noop),
     ]
