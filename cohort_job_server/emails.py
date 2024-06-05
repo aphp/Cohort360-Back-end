d3b961758fede67f0da2162b7c451018c13d5310
Hicham <hicham.taroq-ext@aphp.fr>
2024-06-05 17:37:17 +0200

    refactor(cohort_job_server): rm logging in email notification

--- cohort_job_server/emails.py
+++ cohort_job_server/emails.py
@@ -2,15 +2,13 @@ from django.conf import settings
 
 from admin_cohort.emails import EmailNotification
 from admin_cohort.models import User
-from admin_cohort.settings import FRONT_URL
-from cohort_job_server.utils import _logger
 
 
 def send_email_notif_large_cohort_ready(cohort_name: str, cohort_id: str, cohort_owner: User) -> None:
     context = {"contact_email_address": settings.EMAIL_SUPPORT_CONTACT,
                "recipient_name": cohort_owner.display_name,
                "cohort_name": cohort_name,
-               "cohort_url": f"{FRONT_URL}/cohort/{cohort_id}"
+               "cohort_url": f"{settings.FRONT_URL}/cohort/{cohort_id}"
                }
     email_notif = EmailNotification(subject="Votre cohorte est prête",
                                     to=cohort_owner.email,
@@ -18,4 +16,3 @@ def send_email_notif_large_cohort_ready(cohort_name: str, cohort_id: str, cohort
                                     txt_template="txt/large_cohort_finished.txt",
                                     context=context)
     email_notif.push()
-    _logger.info(f"Email notification sent to user: {cohort_owner}. Cohort [{cohort_name} - {cohort_id}]")
