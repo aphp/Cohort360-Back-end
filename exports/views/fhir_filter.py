7c080154f5d52c0f71a63cc18909376d1545fa29
Hicham <hicham.taroq-ext@aphp.fr>
2024-07-03 12:46:29 +0200

    feat(cohort): flag FHIR filters

--- exports/views/fhir_filter.py
+++ exports/views/fhir_filter.py
@@ -1,5 +1,9 @@
+import json
+
 from django_filters import rest_framework as filters
 from drf_spectacular.utils import extend_schema_view, extend_schema
+from rest_framework import status
+from rest_framework.response import Response
 
 from cohort.models import FhirFilter
 from cohort.permissions import IsOwnerPermission
@@ -25,3 +29,9 @@ class FhirFilterViewSet(ExportsBaseViewSet):
     filterset_class = FhirFilterFilter
     search_fields = ('$name', '$fhir_resource', '$filter')
 
+    def list(self, request, *args, **kwargs):
+        fhir_filters = self.filter_queryset(self.get_queryset())
+        if json.loads(request.query_params.get("pseudo_mode", "false")):
+            fhir_filters = fhir_filters.filter(identifying=False)
+        return Response(data=self.serializer_class(fhir_filters, many=True).data,
+                        status=status.HTTP_200_OK)
