213b3df97502d52b7cd928e4d38fe9849743b283
Paul Bui-Quang <paul.bui-quang@aphp.fr>
2024-07-17 15:00:59 +0200

    refactor(celery): remove beats from docker entry point
    (cherry picked from commit 383b73441c2a2a8b7ebf46e9898a078269326a7a)

--- exports/tasks.py
+++ exports/tasks.py
@@ -3,7 +3,6 @@ import logging
 from celery import shared_task
 
 from admin_cohort.celery import celery_app
-from admin_cohort.tools.celery_periodic_task_helper import ensure_single_task
 from exports.services.export_operators import ExportManager, ExportCleaner
 
 _logger = logging.getLogger('django.request')
@@ -15,6 +14,5 @@ def launch_export_task(export_id: str):
 
 
 @celery_app.task()
-@ensure_single_task("delete_exported_files")
 def delete_exported_files():
     ExportCleaner().delete_exported_files()
