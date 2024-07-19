18fcf71e43842a79fc405f403247fd6eafbcc775
Hicham <hicham.taroq-ext@aphp.fr>
2024-07-19 15:34:06 +0200

    fix(accesses): remove deprecated tasks decorator

--- accesses_perimeters/tasks.py
+++ accesses_perimeters/tasks.py
@@ -1,9 +1,7 @@
 from accesses_perimeters.perimeters_updater import perimeters_data_model_objects_update
 from admin_cohort import celery_app
-from admin_cohort.tools.celery_periodic_task_helper import ensure_single_task
 
 
 @celery_app.task()
-@ensure_single_task("perimeters_daily_update")
 def perimeters_daily_update():
     perimeters_data_model_objects_update()
