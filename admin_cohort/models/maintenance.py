84a2dafdec1ff6ccb639c125317a18f1207e719f
Paul Bui-Quang <paul.bui-quang@aphp.fr>
2024-09-03 12:50:10 +0200

    feat: add type and message to maintenance phase (#416)

--- admin_cohort/models/maintenance.py
+++ admin_cohort/models/maintenance.py
@@ -1,12 +1,27 @@
+from enum import StrEnum
+
 from django.db import models
 from django.utils import timezone
 
 from admin_cohort.models import BaseModel
 
 
+class MaintenanceType(StrEnum):
+    PARTIAL = 'partial'
+    FULL = 'full'
+
+
+MAINTENANCE_TYPE_CHOICES = [
+    (MaintenanceType.PARTIAL, 'Partial'),
+    (MaintenanceType.FULL, 'Full'),
+]
+
+
 class MaintenancePhase(BaseModel):
     id = models.AutoField(primary_key=True)
     subject = models.TextField()
+    message = models.TextField(null=True)
+    type = models.CharField(max_length=10, choices=MAINTENANCE_TYPE_CHOICES, default=MaintenanceType.PARTIAL)
     start_datetime = models.DateTimeField(null=False)
     end_datetime = models.DateTimeField(null=False)
 
