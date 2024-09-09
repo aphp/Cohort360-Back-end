279bcfb594d46ec8797fcfb9ab2a682220a1c892
Hicham <hicham.taroq-ext@aphp.fr>
2024-09-09 09:29:30 +0200

    refactor: better way to check if app is installed

--- cohort/apps.py
+++ cohort/apps.py
@@ -1,11 +1,10 @@
-from django.apps import AppConfig
-from django.conf import settings
+from django.apps import AppConfig, apps
 
 
 class CohortConfig(AppConfig):
     name = 'cohort'
 
-    if "cohort_job_server" in settings.INCLUDED_APPS:
+    if apps.is_installed("cohort_job_server"):
         from cohort_job_server.apps import CohortJobServerConfig
         COHORT_OPERATORS = CohortJobServerConfig.COHORT_OPERATORS
     else:
