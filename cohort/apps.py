8bd16cb2e8acc8d648d67742a1271d68f6b6ff26
Hicham <hicham.taroq-ext@aphp.fr>
2024-09-09 10:31:21 +0200

    refactor: check if app is installed after readiness probe

--- cohort/apps.py
+++ cohort/apps.py
@@ -4,17 +4,18 @@ from django.apps import AppConfig, apps
 class CohortConfig(AppConfig):
     name = 'cohort'
 
-    if apps.is_installed("cohort_job_server"):
-        from cohort_job_server.apps import CohortJobServerConfig
-        COHORT_OPERATORS = CohortJobServerConfig.COHORT_OPERATORS
-    else:
-        COHORT_OPERATORS = [
-            {
-                "TYPE": "count",
-                "OPERATOR_CLASS": "cohort.services.cohort_operators.DefaultCohortCounter"
-            },
-            {
-                "TYPE": "create",
-                "OPERATOR_CLASS": "cohort.services.cohort_operators.DefaultCohortCreator"
-            }
-        ]
+    COHORT_OPERATORS = [
+        {
+            "TYPE": "count",
+            "OPERATOR_CLASS": "cohort.services.cohort_operators.DefaultCohortCounter"
+        },
+        {
+            "TYPE": "create",
+            "OPERATOR_CLASS": "cohort.services.cohort_operators.DefaultCohortCreator"
+        }
+    ]
+
+    def ready(self):
+        if apps.is_installed("cohort_job_server"):
+            from cohort_job_server.apps import CohortJobServerConfig
+            self.COHORT_OPERATORS = CohortJobServerConfig.COHORT_OPERATORS
