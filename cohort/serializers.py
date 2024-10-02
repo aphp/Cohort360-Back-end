c5239f1f25f46b2921a1104f7bfa15d2a97e0886
Hicham <hicham.taroq-ext@aphp.fr>
2024-10-02 09:39:52 +0200

    fix: feasibility study serializers

--- cohort/serializers.py
+++ cohort/serializers.py
@@ -297,9 +297,12 @@ class FhirFilterPatchSerializer(FhirFilterCreateSerializer):
 
 
 class FeasibilityStudySerializer(serializers.ModelSerializer):
+    owner = UserPrimaryKeyRelatedField(queryset=User.objects.all(), required=False)
+
     class Meta:
         model = FeasibilityStudy
         fields = ["uuid",
+                  "owner",
                   "created_at",
                   "request_job_status",
                   "total_count",
@@ -312,6 +315,7 @@ class FeasibilityStudyCreateSerializer(serializers.ModelSerializer):
     class Meta:
         model = FeasibilityStudy
         fields = ["request_query_snapshot"]
+
 
 
 class FeasibilityStudyPatchSerializer(DatedMeasurePatchSerializer):
