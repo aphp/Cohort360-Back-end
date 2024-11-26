4c7a3f3c18c46a7779ba42d6c1ea9e4f36f65490
Paul Bui-Quang <paul.bui-quang@aphp.fr>
2024-11-26 15:40:52 +0100

    refactor(start)!: add new docker entrypoint options

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -4,9 +4,12 @@
 WITH_CELERY_BEAT=""
 WITH_KERBEROS=false
 WITH_INIT=false
+WITH_LOGGING=false
+WITHOUT_APP_SERVER=false
+WITH_DB_MIGRATION=false
 
 # Parse options using getopts
-while getopts "bki" opt; do
+while getopts "bkilnd" opt; do
     case $opt in
         b)
             WITH_CELERY_BEAT="-B"
@@ -16,6 +19,15 @@ while getopts "bki" opt; do
             ;;
         i)
             WITH_INIT=true
+            ;;
+        l)
+            WITH_LOGGING=true
+            ;;
+        n)
+            WITHOUT_APP_SERVER=true
+            ;;
+        d)
+            WITH_DB_MIGRATION=true
             ;;
         \?)
             echo "Invalid option: -$OPTARG" >&2
@@ -40,8 +52,11 @@ sed -i s/{{BACK_HOST}}/"$BACK_HOST"/g /etc/nginx/nginx.conf;
 service nginx restart
 
 source $VIRTUAL_ENV/bin/activate
-python manage.py migrate --database="default"
-python manage.py collectstatic --noinput
+
+if [ "$WITH_DB_MIGRATION" = true ]; then
+  python manage.py migrate --database="default"
+  python manage.py collectstatic --noinput
+fi
 
 if [ "$WITH_INIT" = true ]; then
   set +e
@@ -56,16 +71,21 @@ if [ "$WITH_KERBEROS" = true ]; then
   cron
 fi
 
-celery -A admin_cohort worker $WITH_CELERY_BEAT --loglevel=INFO --logfile=/app/log/celery.log &
-sleep 5
+if [ "$WITH_LOGGING" = true ]; then
+  python setup_logging.py &
+fi
 
-python setup_logging.py &
-# For websockets
-daphne -p 8005 admin_cohort.asgi:application &
+if [ "$WITHOUT_APP_SERVER" = false ]; then
+  celery -A admin_cohort worker $WITH_CELERY_BEAT --loglevel=INFO --logfile=/app/log/celery.log &
+  sleep 5
 
-gunicorn admin_cohort.wsgi --config .conf/gunicorn.conf.py
+  # For websockets
+  daphne -p 8005 admin_cohort.asgi:application &
 
-tail -f /app/log/django.error.log
+  gunicorn admin_cohort.wsgi --config .conf/gunicorn.conf.py
+
+  tail -f /app/log/django.error.log
+fi
 
 # Wait for any process to exit
 wait -n
