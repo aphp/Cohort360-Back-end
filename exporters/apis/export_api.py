75d07b39fd7265661c38ec8bc22d916729138fe0
Hicham <hicham.taroq-ext@aphp.fr>
2024-12-06 10:25:38 +0100

    fix(exports): create in memory yaml file

--- exporters/apis/export_api.py
+++ exporters/apis/export_api.py
@@ -1,17 +1,14 @@
 import logging
-import os
-from pathlib import Path
+from io import StringIO
 from uuid import UUID
 
 import requests
-from rest_framework import status
+import yaml
 
 from exporters.apis.base import BaseAPI
-from exporters.utils import build_yaml
+
 
 _logger = logging.getLogger('django.request')
-
-EXPORT_YAML_FILE = Path(__file__).resolve().parent.parent / 'export-%s.yml'
 
 
 class ExportAPI(BaseAPI):
@@ -22,14 +19,13 @@ class ExportAPI(BaseAPI):
         self.required_table = "person"
 
     def launch_export(self, export_id: UUID, params: dict) -> str:
-        file_path = str(EXPORT_YAML_FILE) % export_id
-        _logger.info(f"Export [{export_id}] temporarily storing the yaml file at: {file_path}")
-        file_saved = build_yaml(dict_in=params, outfile_path=file_path)
-        if file_saved:
-            response = requests.post(url=f"{self.url}/yaml",
-                                     files={'yaml_file': open(file_path, 'rb')},
-                                     headers={'auth-token': self.auth_token})
-            if response.status_code == status.HTTP_200_OK:
-                os.remove(file_path)
-            return response.json().get('task_id')
-        raise FileNotFoundError(f"Export [{export_id}] Error saving the yaml config file")
+        try:
+            yaml_data = yaml.dump(params, default_flow_style=False, sort_keys=False)
+            yaml_file = StringIO(yaml_data)
+        except yaml.YAMLError as e:
+            _logger.error(f"Export[{export_id}] Error generating the yaml config from export params")
+            raise e
+        response = requests.post(url=f"{self.url}/yaml",
+                                 files={"yaml_file": ("yaml_config.yaml", yaml_file, "application/x-yaml")},
+                                 headers={'auth-token': self.auth_token})
+        return response.json().get('task_id')
