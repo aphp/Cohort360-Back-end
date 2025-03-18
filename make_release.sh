1864eef144336713d8cdc196587fda90b78a4f91
Hicham <hicham.taroq-ext@aphp.fr>
2025-03-18 16:42:13 +0100

    fix(release): set tags pattern and fix script

--- make_release.sh
+++ make_release.sh
@@ -3,7 +3,7 @@
 set -e
 
 if [ $# -eq 0 ]; then
-  echo "-------------- Missing release version argument"
+  echo "Missing release version argument"
   exit 1
 fi
 
@@ -15,30 +15,31 @@ PYPROJECT_FILE="pyproject.toml"
 echo "-------------- Preparing a new release: $RELEASE_VERSION"
 
 if git show-ref --verify --quiet refs/heads/"$SOURCE_BRANCH"; then
-    git checkout "$SOURCE_BRANCH" || { echo "-------------- Failed to switch to '$SOURCE_BRANCH' branch."; exit 1; }
-    git pull origin "$SOURCE_BRANCH" || { echo "-------------- Failed to update '$SOURCE_BRANCH'."; exit 1; }
-    git checkout -b "$RELEASE_BRANCH"
+    git checkout "$SOURCE_BRANCH" || { echo "Failed to switch to '$SOURCE_BRANCH' branch."; exit 1; }
+    git pull origin "$SOURCE_BRANCH" || { echo "Failed to update '$SOURCE_BRANCH'."; exit 1; }
 else
-    echo "-------------- Branch '$SOURCE_BRANCH' does not exist. Aborting."
+    echo "Branch '$SOURCE_BRANCH' does not exist. Aborting."
     exit 1
 fi
 
 sed -i "/^version = /s/\".*\"/\"$RELEASE_VERSION\"/" "$PYPROJECT_FILE"
-
-echo "-------------- $PYPROJECT_FILE updated with [version = $RELEASE_VERSION]"
+echo "$PYPROJECT_FILE updated with [version = $RELEASE_VERSION]"
 
 git add "$PYPROJECT_FILE"
 git commit -m "new release $RELEASE_VERSION"
 git tag "$RELEASE_VERSION"
-echo "-------------- New tag created: $RELEASE_VERSION"
+echo "New tag created: $RELEASE_VERSION"
 
-echo "-------------- Pushing a new branch: $RELEASE_BRANCH and tag: $RELEASE_VERSION"
-git push --set-upstream origin "$RELEASE_BRANCH" --dry-run -v
+echo "Create release branch $RELEASE_BRANCH from tag $RELEASE_VERSION"
+git checkout -b "$RELEASE_BRANCH" "$RELEASE_VERSION"
+
+echo "Pushing release branch: $RELEASE_BRANCH and tag: $RELEASE_VERSION"
+git push --set-upstream origin "$RELEASE_BRANCH" -v --dry-run
 
 git checkout $SOURCE_BRANCH
 
+echo "-------------- Update CHANGELOG.md and set next dev version"
 git cliff --latest --prepend CHANGELOG.md
-echo "-------------- updated changelog"
 
 # prepare next dev version
 IFS='.' read -r MAJOR MINOR PATCH <<< "$RELEASE_VERSION"
@@ -49,3 +50,6 @@ sed -i "/^version = /s/\".*\"/\"$NEXT_DEV_VERSION\"/" "$PYPROJECT_FILE"
 
 git add "$PYPROJECT_FILE" CHANGELOG.md
 git commit -m "chore: update CHANGELOG.md and bump version to $NEXT_DEV_VERSION"
+
+echo "Pushing branch $SOURCE_BRANCH"
+git push --dry-run
