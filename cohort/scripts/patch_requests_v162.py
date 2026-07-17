343834ac0cf7a72383e9b800aeb71d40fc0fb6a5
Rony <cohen.rony@gmail.com>
2026-07-17 12:15:13 +0200

    fix(scripts): remove obsolete OMOP dependency from v162

--- cohort/scripts/patch_requests_v162.py
+++ cohort/scripts/patch_requests_v162.py
@@ -1,14 +1,5 @@
-import logging
-import sys
-
-from django.db import connections
-
 from cohort.scripts.patch_requests_v161 import NEW_VERSION as PREV_VERSION
-from cohort.scripts.query_request_updater import RESOURCE_DEFAULT, MATCH_ALL_VALUES, QueryRequestUpdater, find_mapped_code
-
-LOGGER = logging.getLogger(__name__)
-stream_handler = logging.StreamHandler(stream=sys.stdout)
-LOGGER.addHandler(stream_handler)
+from cohort.scripts.query_request_updater import RESOURCE_DEFAULT, MATCH_ALL_VALUES, QueryRequestUpdater
 
 NEW_VERSION = "v1.6.2"
 
@@ -18,8 +9,6 @@ FILTER_NAME_TO_SKIP = {}
 
 CCAM_OLD_CODESYSTEM = "https://www.atih.sante.fr/plateformes-de-transmission-et-logiciels/logiciels-espace-de-telechargement/id_lot/3550"
 CCAM_NEW_CODESYSTEM = "https://aphp.fr/ig/fhir/core/CodeSystem/CCAMDescriptiveVerAPHP"
-
-code_mapping_cache = {}
 
 
 def map_ccam_code_token(code_token: str) -> str:
@@ -36,18 +25,10 @@ def map_ccam_code_token(code_token: str) -> str:
     else:
         code = token
 
-    return find_mapped_code(
-        code,
-        CCAM_OLD_CODESYSTEM,
-        CCAM_NEW_CODESYSTEM,
-        lambda c: f"{CCAM_NEW_CODESYSTEM}|{c}",
-        connections["omop"],
-        code_mapping_cache,
-    )
+    return f"{CCAM_NEW_CODESYSTEM}|{code}"
 
 
 def map_ccam_codes(codes: str) -> str:
-    LOGGER.info(f"Translating CCAM codes {codes}")
     return ",".join([map_ccam_code_token(code_token) for code_token in codes.split(",")])
 
 
